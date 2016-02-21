6c90a53f32820400050bf9ca30079ff9927c118d
al-fisher <alex.fisher@burohappold.com>
2016-02-21 18:15:58 +0000

    Added initial skeleton plugin
    Skeleton Alligator (scary!)
    Basic AboutGatorComponent added
    TODO: - link with BHoM - move some common utility functions to BHoM or
    similar

---
+++ Alligator/AlligatorInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+
+namespace Alligator
+{
+    public class AlligatorInfo : GH_AssemblyInfo
+    {
+        public override string Name
+        {
+            get
+            {
+                return "Alligator";
+            }
+        }
+        public override Bitmap Icon
+        {
+            get
+            {
+                //Return a 24x24 pixel bitmap to represent this GHA library.
+                return null;
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                //Return a short string describing the purpose of this GHA library.
+                return "BuroHappold Computational Engineering for Grasshopper";
+            }
+        }
+        public override Guid Id
+        {
+            get
+            {
+                return new Guid("4e66e70a-ce50-46a9-a146-93c151e94b88");
+            }
+        }
+
+        public override string AuthorName
+        {
+            get
+            {
+                //Return a string identifying you or your company.
+                return "BuroHappold Engineering";
+            }
+        }
+        public override string AuthorContact
+        {
+            get
+            {
+                //Return a string representing your preferred contact details.
+                return "";
+            }
+        }
+    }
+}
