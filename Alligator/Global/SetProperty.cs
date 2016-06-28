66da54135f08c6f08023dce5df4f9d9943c2e1c0
adecler <arnaud.declercq@burohappold.com>
2016-06-28 16:39:38 +0100

    SetProperty component

--- Alligator/Global/SetProperty.cs
+++ Alligator/Global/SetProperty.cs
@@ -7,15 +7,15 @@ using Grasshopper.Kernel;
 
 namespace Alligator.Global
 {
-    public class GetProperty : GH_Component
+    public class SetProperty : GH_Component
     {
-        public GetProperty() : base("GetProperty", "GetProperty", "Get property of a BHoM object from the property name", "Alligator", "Global") { }
+        public SetProperty() : base("SetProperty", "SetProperty", "Set property of a BHoM object", "Alligator", "Global") { }
 
         public override Guid ComponentGuid
         {
             get
             {
-                return new Guid("E14EF77D-4F09-4CFB-AB75-F9B723212D00");
+                return new Guid("E3C42F6C-15AC-4FBA-8BCC-F3E773B1C1D8");
             }
         }
 
@@ -23,23 +23,31 @@ namespace Alligator.Global
         {
             pManager.AddGenericParameter("BHoM object", "object", "BHoM object to convert", GH_ParamAccess.item);
             pManager.AddTextParameter("key", "key", "Property name", GH_ParamAccess.item);
+            pManager.AddGenericParameter("value", "value", "Property value", GH_ParamAccess.item);
         }
 
         protected override void RegisterOutputParams(GH_OutputParamManager pManager)
         {
-            pManager.AddGenericParameter("Property value", "value", "Value of the property", GH_ParamAccess.item);
+            pManager.AddGenericParameter("BHoM object", "object", "BHoM object to convert", GH_ParamAccess.item);
         }
 
         protected override void SolveInstance(IGH_DataAccess DA)
         {
             object o = Utils.GetGenericData<object>(DA, 0);
             string key = Utils.GetData<string>(DA, 1);
+            object value = Utils.GetGenericData<object>(DA, 2);
 
             System.Reflection.PropertyInfo prop = o.GetType().GetProperty(key);
             if (prop == null)
+            {
                 DA.SetData(0, null);
+            }
             else
-                DA.SetData(0, prop.GetValue(o));
+            {
+                prop.SetValue(o, value);
+                DA.SetData(0, o);
+            }
+                
         }
     }
 }
