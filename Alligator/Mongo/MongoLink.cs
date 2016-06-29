fa438bd223b6784f20a3cb84a159b1eae23b44bd
adecler <arnaud.declercq@burohappold.com>
2016-06-29 15:35:45 +0100

    Adding mongo functionality

---
+++ Alligator/Mongo/MongoLink.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grasshopper.Kernel;
+
+namespace Alligator.Mongo
+{
+    public class MongoLink : GH_Component
+    {
+        public MongoLink() : base("MongoLink", "MongoLink", "Create a link to a Mongo database", "Alligator", "Mongo") { }
+
+        public override Guid ComponentGuid
+        {
+            get
+            {
+                return new Guid("7E81BE63-D7FC-4EC9-A41D-CAFB7B1097A7");
+            }
+        }
+
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddTextParameter("server", "server", "address of the server", GH_ParamAccess.item, "mongodb://host:27017");
+            pManager.AddTextParameter("database", "database", "name of the database", GH_ParamAccess.item, "project");
+            pManager.AddTextParameter("collection", "collection", "name of the collection", GH_ParamAccess.item, "bhomObjects");
+        }
+
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("link", "link", "link to the database colelction", GH_ParamAccess.item);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            string server = Utils.GetData<string>(DA, 0);
+            string database = Utils.GetData<string>(DA, 1);
+            string collection = Utils.GetData<string>(DA, 2);
+
+            DA.SetData(0, new BHoM_Engine.Databases.Mongo.MongoLink(server, database, collection));
+        }
+    }
+}
---
+++ Alligator/Mongo/ToMongo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grasshopper.Kernel;
+
+namespace Alligator.Mongo
+{
+    public class ToMongo : GH_Component
+    {
+        public ToMongo() : base("ToMongo", "ToMongo", "Send BHoM objects to a Mongo database", "Alligator", "Mongo") { }
+
+        public override Guid ComponentGuid
+        {
+            get
+            {
+                return new Guid("30CA2D65-C265-43A5-A7FA-E183C2B916EB");
+            }
+        }
+
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Mongo link", "link", "collection to send the data to", GH_ParamAccess.item);
+            pManager.AddGenericParameter("BHoM objects", "objects", "BHoM objects to convert", GH_ParamAccess.list);
+        }
+
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            BHoM_Engine.Databases.Mongo.MongoLink link = Utils.GetGenericData<BHoM_Engine.Databases.Mongo.MongoLink>(DA, 0);
+            List<BHoM.Global.BHoMObject> bhomObjects = Utils.GetGenericDataList<BHoM.Global.BHoMObject>(DA, 1);
+
+            link.SaveObjects(bhomObjects);
+        }
+    }
+}
