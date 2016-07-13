1c00dbc797cd05498800aff71c0bf63a8901e8d1
johannaisak <jomiisak@gmail.com>
2016-07-13 14:37:56 +0100

    Updates to GSA
    Updates to GSA

--- GSA/GSA_Application.cs
+++ GSA/GSA_Application.cs
@@ -8,39 +8,44 @@ using Grasshopper.Kernel;
 
 namespace Alligator.GSA
 {
-    //public class GSAApp : GH_Component
-    //{
-    //    public GSAApp() : base("GSA Application", "GSAApp", "Creates a GSA Application", "GSA", "Application") { }
+    public class GSAApp : GH_Component
+    {
+        public GSAApp() : base("GSA Application", "GSAApp", "Creates a GSA Application", "GSA", "Application") { }
 
-    //    protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
-    //    {
-    //        pManager.AddTextParameter("Filename", "F", "GSA Filename", GH_ParamAccess.item);
-    //        pManager.AddGenericParameter("Settings", "S", "Application Settings", GH_ParamAccess.item);
-    //        pManager[0].Optional = true;
-    //        pManager[1].Optional = true;
-    //    }
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddTextParameter("Filename", "F", "GSA Filename", GH_ParamAccess.item);
+            pManager.AddGenericParameter("Settings", "S", "Application Settings", GH_ParamAccess.item);
+            pManager[0].Optional = true;
+            pManager[1].Optional = true;
+        }
 
-    //    protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
-    //    {
-    //        pManager.AddGenericParameter("Application", "A", "GSA Application", GH_ParamAccess.item);
-    //    }
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Application", "A", "GSA Application", GH_ParamAccess.item);
+        }
 
-    //    protected override void SolveInstance(IGH_DataAccess DA)
-    //    {
-    //        RobotAdapter app = new RobotAdapter();
-    //        DA.SetData(0, app);
-    //    }
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            string filePath = "";
+            GSAAdapter app;
+            if (DA.GetData(0, ref filePath))
+                app = new GSAAdapter(filePath);
+            else
+                app = new GSAAdapter();
+            DA.SetData(0, app);
+        }
 
-    //    public override Guid ComponentGuid
-    //    {
-    //        get { return new Guid("97b9ded4-41a2-4ded-b835-432332fb760a"); }
-    //    }
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("92ee004b-afe3-4ed3-b2d2-73248f8578d6"); }
+        }
 
-    //    /// <summary> Icon (24x24 pixels)</summary>
-    //    protected override System.Drawing.Bitmap Internal_Icon_24x24
-    //    {
-    //        get { return Robot.Properties.Resources.robot_app; }
-    //    }
+        /// <summary> Icon (24x24 pixels)</summary>
+        protected override System.Drawing.Bitmap Internal_Icon_24x24
+        {
+            get { return GSA.Properties.Resources.gsa_app; }
+        }
 
-    //}
+    }
 }
