68a1f308af90cf1facdfe9e9e5c8fc6fe75b06ba
adecler <arnaud.declercq@burohappold.com>
2016-08-10 16:53:06 +0100

    For Eddy (doesn't compile yet)

---
+++ Robot_Alligator/Elements/Node.cs
@@ -0,0 +1,63 @@
+﻿using Grasshopper.Kernel;
+using System;
+using System.Collections.Generic;
+using GHE = Grasshopper_Engine;
+using BHI = BHoM.Structural.Interface;
+using BHE = BHoM.Structural.Elements;
+
+
+namespace Alligator.Robot.Elements
+{
+    public class SetNode : GH_Component
+    {
+        public SetNode() : base("Set Node", "ExNode", "Create a node", "Robot", "Elements") { }
+
+        public override GH_Exposure Exposure
+        {
+            get
+            {
+                return GH_Exposure.secondary;
+            }
+        }
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Application", "App", "Application to import nodes from", GH_ParamAccess.item);
+            pManager.AddGenericParameter("Node", "N", "BHoM Node", GH_ParamAccess.list);
+            pManager.AddBooleanParameter("Execute", "R", "Set Geometry", GH_ParamAccess.item);
+            pManager[2].AddVolatileData(new Grasshopper.Kernel.Data.GH_Path(0), 0, false);
+        }
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("Id", "Id", "Node Id", GH_ParamAccess.list); ;
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            if (GHE.DataUtils.Run(DA, 2))
+            {
+                BHI.IElementAdapter app = GHE.DataUtils.GetGenericData<BHI.IElementAdapter>(DA, 0);
+                if (app != null)
+                {
+                    List<BHE.Node> nodes = GHE.DataUtils.GetGenericDataList<BHE.Node>(DA, 1);
+                    List<string> ids = null;
+                    app.SetNodes(nodes, out ids);
+
+                    DA.SetDataList(0, ids);
+                }
+            }
+        }
+
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("322adc1f-86b4-491a-93fa-1495515ca5aa"); }
+        }
+
+        /// <summary> Icon (24x24 pixels)</summary>
+        protected override System.Drawing.Bitmap Internal_Icon_24x24
+        {
+            get { return Robot_Alligator.Properties.Resources.node; }
+        }
+    }
+}
---
+++ Robot_Alligator/Elements/Panel.cs
@@ -0,0 +1,59 @@
+﻿using Grasshopper.Kernel;
+using System;
+using System.Collections.Generic;
+using GHE = Grasshopper_Engine;
+using BHI = BHoM.Structural.Interface;
+using BHE = BHoM.Structural.Elements;
+
+
+namespace Alligator.Robot.Elements
+{
+    public class SetPanel : GH_Component
+    {
+        public SetPanel() : base("Set Panel", "ExPanel", "Create a panel", "Robot", "Elements") { }
+
+        public override GH_Exposure Exposure
+        {
+            get
+            {
+                return GH_Exposure.secondary;
+            }
+        }
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Application", "App", "Application to import panels from", GH_ParamAccess.item);
+            pManager.AddGenericParameter("Panel", "N", "BHoM Panel", GH_ParamAccess.list);
+            pManager.AddBooleanParameter("Execute", "R", "Set Geometry", GH_ParamAccess.item);
+            pManager[2].AddVolatileData(new Grasshopper.Kernel.Data.GH_Path(0), 0, false);
+        }
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("Id", "Id", "Panel Id", GH_ParamAccess.list); ;
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            if (GHE.DataUtils.Run(DA, 2))
+            {
+                BHI.IElementAdapter app = GHE.DataUtils.GetGenericData<BHI.IElementAdapter>(DA, 0);
+                if (app != null)
+                {
+                    List<BHE.Panel> panels = GHE.DataUtils.GetGenericDataList<BHE.Panel>(DA, 1);
+                    List<string> ids = null;
+                    app.SetPanels(panels, out ids);
+
+                    DA.SetDataList(0, ids);
+                }
+            }
+        }
+
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("61B66CED-C4D5-45F1-8EB4-A6AA3EDD6C88"); }
+        }
+
+        
+    }
+}
