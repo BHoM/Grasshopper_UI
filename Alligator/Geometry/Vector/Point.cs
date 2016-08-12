23c8078cf2a183147b4c2ea212f749e905d67761
adecler <arnaud.declercq@burohappold.com>
2016-08-12 16:19:04 +0100

    some interface for creating BHoM geometry and for additional model laundry

---
+++ Alligator/Geometry/Vector/Point.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grasshopper.Kernel;
+using RG = Rhino.Geometry;
+using BHG = BHoM.Geometry;
+
+namespace Alligator.Geometry
+{
+    public class Point : GH_Component
+    {
+        public Point() : base("BHPoint", "BHPoint", "Create a BHoM Point", "Alligator", "geometry") { }
+
+        public override Guid ComponentGuid
+        {
+            get
+            {
+                return new Guid("59458E1C-5E42-4A84-B6B7-3DC3F185D355");
+            }
+        }
+
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddPointParameter("point", "point", "GH point", GH_ParamAccess.item);
+        }
+
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Point", "Point", "BHoM Point", GH_ParamAccess.item);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            RG.Point3d inPt = new RG.Point3d();
+
+            DA.GetData<RG.Point3d>(0, ref inPt);
+
+            BHG.Point Point = new BHG.Point(inPt.X, inPt.Y, inPt.Z);
+
+            DA.SetData(0, Point);
+        }
+    }
+}
---
+++ ModelLaundry_Alligator/BarToPanel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Grasshopper.Kernel;
+using MLE = ModelLaundry_Engine;
+using GHE = Grasshopper_Engine;
+using BHE = BHoM.Structural.Elements;
+
+
+namespace ModelLaundry_Alligator
+{
+    public class BarToPanel : GH_Component
+    {
+        public BarToPanel() : base("BarToPanel", "BarToPanel", "Convert bars into panels", "Alligator", "ModelLaundry") { }
+
+        public override Guid ComponentGuid
+        {
+            get
+            {
+                return new Guid("42ACF2BC-60F9-4166-AA25-888DB36E3A8B");
+            }
+        }
+
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("bars", "bars", "bars to be converted", GH_ParamAccess.list);
+        }
+
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("panels", "panels", "resulting panels", GH_ParamAccess.list);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            List<BHE.Bar> bars = GHE.DataUtils.GetDataList<BHE.Bar>(DA, 0);
+            List<BHE.Panel> panels = MLE.Structure.BarsToPanels(bars);
+            DA.SetDataList(0, panels);
+        }
+    }
+}
