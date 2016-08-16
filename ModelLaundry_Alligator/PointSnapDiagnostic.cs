6e2d140f1731bb57833b0a9df5883b1f075e2920
adecler <arnaud.declercq@burohappold.com>
2016-08-16 12:00:33 +0100

    snapping diagnostic for points

---
+++ ModelLaundry_Alligator/PointSnapDiagnostic.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using Grasshopper.Kernel;
+using MLE = ModelLaundry_Engine;
+using GHE = Grasshopper_Engine;
+using BHG = BHoM.Geometry;
+using RG = Rhino.Geometry;
+
+namespace Alligator.ModelLaundry
+{
+    public class PointSnapDiagnostic : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the HorizontalExtend class.
+        /// </summary>
+        public PointSnapDiagnostic() : base("PointSnapDiagnostic", "PSDiagnostic", "Check that oints within tolerance are correctly snapped", "Alligator", "ModelLaundry") {}
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("BHoM Elements", "bhElements", "BHoM object or geometry to diagnose", GH_ParamAccess.list);
+            pManager.AddNumberParameter("Distance", "distance", "tolerance distance", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddPointParameter("Error locations", "errors", "Locations where two points are not snapped properly", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            // Getting the inputs from GH
+            List<object> elements = GHE.DataUtils.GetGenericDataList<object>(DA, 0);
+            double dist = GHE.DataUtils.GetData<double>(DA, 1);
+
+            List<BHG.Point> result = MLE.Diagnostic.CheckSnappedPoints(elements, dist);
+            List<RG.Point3d> locations = new List<Rhino.Geometry.Point3d>();
+            foreach (BHG.Point pt in result)
+                locations.Add(new RG.Point3d(pt.X, pt.Y, pt.Z));
+            DA.SetDataList(0, locations);
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("42663B13-33AC-4E0D-8D5C-BF25F3955EAD"); }
+        }
+    }
+}
