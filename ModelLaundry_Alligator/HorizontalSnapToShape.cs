a40056c8c2f544851d29a2ffb99e624e6817e736
adecler <arnaud.declercq@burohappold.com>
2016-08-18 11:00:58 +0100

    adding the ability to use objects from other floors for snapping

--- ModelLaundry_Alligator/HorizontalSnapToShape.cs
+++ ModelLaundry_Alligator/HorizontalSnapToShape.cs
@@ -27,6 +27,7 @@ namespace Alligator.ModelLaundry
             pManager.AddGenericParameter("BHoM Elements", "bhElements", "BHoM object or geometry to snap", GH_ParamAccess.item);
             pManager.AddGenericParameter("Reference BHoM Elements", "refBHElem", "reference BHoM object or geometry to snap to", GH_ParamAccess.list);
             pManager.AddNumberParameter("Tolerance", "tolerance", "Set a tolerance for the snapping", GH_ParamAccess.item, 0.2);
+            pManager.AddBooleanParameter("AnyHeight", "anyheight", "Snap to a vertical projection of the reference elements", GH_ParamAccess.item, false);
         }
 
         /// <summary>
@@ -46,8 +47,9 @@ namespace Alligator.ModelLaundry
             object element = GHE.DataUtils.GetGenericData<object>(DA, 0);
             List<object> refElements = GHE.DataUtils.GetGenericDataList<object>(DA, 1);
             double tol = GHE.DataUtils.GetData<double>(DA, 2);
+            bool anyHeight = GHE.DataUtils.GetData<bool>(DA, 3);
 
-            object result = MLE.Snapping.HorizontalSnapToShape(element, refElements, tol);
+            object result = MLE.Snapping.HorizontalSnapToShape(element, refElements, tol, anyHeight);
             DA.SetData(0, result);
         }
 
