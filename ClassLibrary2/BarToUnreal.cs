66062b44fc189ccb8e10fc495016018713869087
oscarborgstrom <Oscar.Borgstrom@burohappold.com>
2017-01-03 17:49:52 +0000

    Unreal_Toolkit Update

--- ClassLibrary2/BarToUnreal.cs
+++ ClassLibrary2/BarToUnreal.cs
@@ -14,7 +14,7 @@ namespace Alligator.Unreal
 {
     public class BarToUnreal : GH_Component
     {
-        public BarToUnreal() : base("BarToUnreal", "BarToUnreal", "Convert BHoM Bar into Unreal Message", "Alligator", "unreal") { }
+        public BarToUnreal() : base("BarToUnreal", "BarToUnreal", "Convert BHoM Bar into Unreal Message", "Alligator", "Unreal") { }
 
         public override Guid ComponentGuid
         {
@@ -36,75 +36,30 @@ namespace Alligator.Unreal
 
         protected override void RegisterOutputParams(GH_OutputParamManager pManager)
         {
-            pManager.AddTextParameter("BarMessage", "BarMesssage", "Converted Bar Message", GH_ParamAccess.item);
-            pManager.AddBrepParameter("UnrealBarBrep", "UnrealBarBrep", "Brep to build in Unreal", GH_ParamAccess.list);
-            pManager.AddTextParameter("UnrealBarNames", "UnrealBarNames", "Name for Unreal Bar", GH_ParamAccess.list);
+            pManager.AddTextParameter("Bar Message", "Bar Messsage", "Converted Bar Message", GH_ParamAccess.item);
+            pManager.AddMeshParameter("Unreal Bar Brep", "Unreal Bar Brep", "Brep to build in Unreal", GH_ParamAccess.list);
+            pManager.AddTextParameter("Unreal Bar Names", "Unreal Bar Names", "Name for Unreal Bar", GH_ParamAccess.list);
 
 
         }
 
         protected override void SolveInstance(IGH_DataAccess DA)
         {
+
             List<BHoM.Structural.Elements.Bar> barList = DataUtils.GetDataList<Bar>(DA, 0);
             List<string> Colors = DataUtils.GetDataList<string>(DA, 1);
             string Material = DataUtils.GetData<string>(DA, 2);
-            //Add Message Type
-            string json = "[[[BHoMBars]]]";
-
-            //Add Material Message
-            json += ",[[[" + Material + "]]]";
-
-            //Add SectionProperty Message
-            json += ",[[";
-            for (int i = 0; i < barList.Count; i++)
-            {
-                json += "[" + barList[i].SectionProperty.Name + "],";
-            }
-            json = json.Trim(',') + "]]";
-
-            //Add Color Message
-            json += ",[[";
-            if (0 >= Colors.Count)
-            {
-                json += "[],";
-            }
-            else
-            {
-                for (int i = 0; i < Colors.Count; i++)
-                {
-
-                    List<float> colorValues = new List<float>();
-                    List<string> colorStrings = Colors[i].Split(new Char[] { ',' }, StringSplitOptions.None).ToList(); ;
-                    float colorScale = 1 / 255;
-                    json += "[";
-                    for (int j = 0; j < colorStrings.Count; j++)
-                    {
-                        colorValues.Add(float.Parse(colorStrings[i]) * colorScale);
-                        json += colorValues[j].ToString() + ",";
-                    }
-                    json = json.Trim(',') + "],";
-                }
-            }
-            json = json.Trim(',') + "]]";
-
-            //Add Bar Message.
-            json += ",[";
-            for (int i = 0; i < barList.Count; i++)
-            {
-                json += "[[" + barList[i].StartPoint.X + "," + barList[i].StartPoint.Y + "," + barList[i].StartPoint.Z + "],[" + barList[i].EndPoint.X + "," + barList[i].EndPoint.Y + "," + barList[i].EndPoint.Z + "]],";
-            }
-            json = json.Trim(',') + "]";
-
-            DA.SetData(0, json);
+            int decimals = 0;
 
             //Create Unique BarBreps
-            List<R.Brep> UnrealBrepList = new List<R.Brep>();
+            List<int> SectionPropertyIndex = new List<int>();
+            List<R.Mesh> UnrealMeshList = new List<R.Mesh>();
             List<string> SectionProperties = new List<string>();
             for (int i = 0; i < barList.Count; i++)
             {
                 if (barList[i] != null && barList[i].SectionProperty != null)
                 {
-                    if (SectionProperties.Contains(barList[i].SectionProperty.Name))
+                    if (SectionProperties.Contains(barList[i].SectionProperty.Name.Replace(" ", string.Empty)))
                     { }
                     else
                     {
@@ -117,13 +72,128 @@ namespace Alligator.Unreal
                         R.Curve perimeter = GeometryUtils.Convert(curves[0]);
                         R.Brep UnrealBrep = GeometryUtils.ExtrudeAlong(perimeter, genCentreline, new R.Plane(R.Point3d.Origin, R.Vector3d.XAxis, R.Vector3d.YAxis))[0].ToBrep().CapPlanarHoles(0.01);
                         UnrealBrep.Translate(new R.Vector3d(0, barList[i].SectionProperty.CentreY * -1, 0));
-                        UnrealBrepList.Add(UnrealBrep);
-                        SectionProperties.Add(barList[i].SectionProperty.Name);
+                        List<R.Mesh> UnrealMeshParts = new List<Rhino.Geometry.Mesh>();
+                        UnrealMeshParts = R.Mesh.CreateFromBrep(UnrealBrep, R.MeshingParameters.Coarse).ToList();
+                        R.Mesh UnrealMesh = new Rhino.Geometry.Mesh();
+                        foreach (R.Mesh partialMesh in UnrealMeshParts)
+                        {
+                            UnrealMesh.Append(partialMesh);
+                        }
+                        UnrealMeshList.Add(UnrealMesh);
+                        SectionProperties.Add(barList[i].SectionProperty.Name.Replace(" ", string.Empty));
                     }
                 }
             }
-            DA.SetDataList(1, UnrealBrepList);
+            DA.SetDataList(1, UnrealMeshList);
             DA.SetDataList(2, SectionProperties);
+
+
+            //Add Message Type
+            string json = "[[[BHoMBars]]]";
+
+
+            //Add Material Message
+            json += ",[[[" + Material + "]]]";
+
+
+            //Add SectionProperty Message
+            json += ",[[";
+            for (int i = 0; i < SectionProperties.Count; i++)
+            {
+                json += "[" + SectionProperties[i] + "],";
+            }
+            json = json.Trim(',') + "]]";
+
+
+            //Add Color Message
+            List<int> ColorIndex = new List<int>();
+            List<string> ColorNames = new List<string>();
+            json += ",[[";
+            if (0 >= Colors.Count)
+            {
+                json += "[],";
+            }
+            else
+            {
+                for (int i = 0; i < Colors.Count; i++)
+                {
+
+                    List<string> colorStrings = Colors[i].Split(new Char[] { ',' }, StringSplitOptions.None).ToList();
+                    double colorScale = 1.00 / 255.00;
+                    string ColorName = "[";
+                    for (int j = 0; j < colorStrings.Count; j++)
+                    {
+                        double colorValue = Math.Round(colorScale * double.Parse(colorStrings[j]), 3);
+                        ColorName += colorValue + ",";
+                    }
+                    ColorName = ColorName.Trim(',') + "]";
+
+                    if (ColorNames.Contains(ColorName))
+                    {
+                        ColorIndex.Add(ColorNames.IndexOf(ColorName));
+                    }
+                    else
+                    {
+                        ColorNames.Add(ColorName);
+                        ColorIndex.Add(ColorNames.IndexOf(ColorName));
+                    }
+
+                }
+                for (int i = 0; i < ColorNames.Count; i++)
+                {
+                    json += ColorNames[i] + ",";
+                }
+            }
+            json = json.Trim(',') + "]]";
+
+
+            //Add Points Message.
+            List<string> Points = new List<string>();
+            List<int> StartPointIndex = new List<int>();
+            List<int> EndPointIndex = new List<int>();
+            for (int i = 0; i < barList.Count; i++)
+            {
+                string pointstring = "[" + Math.Round(barList[i].StartPoint.X, decimals, MidpointRounding.AwayFromZero) + "," + Math.Round(barList[i].StartPoint.Y, decimals, MidpointRounding.AwayFromZero) + "," + Math.Round(barList[i].StartPoint.Z, decimals, MidpointRounding.AwayFromZero) + "]";
+
+                if (Points.Contains(pointstring))
+                { StartPointIndex.Add(Points.IndexOf(pointstring));
+                    }
+                else
+                {
+                    Points.Add(pointstring);
+                    StartPointIndex.Add(Points.IndexOf(pointstring));
+                }
+
+                pointstring = "[" + Math.Round(barList[i].EndPoint.X, decimals, MidpointRounding.AwayFromZero) + "," + Math.Round(barList[i].EndPoint.Y, decimals, MidpointRounding.AwayFromZero) + "," + Math.Round(barList[i].EndPoint.Z, decimals, MidpointRounding.AwayFromZero) + "]";
+                if (Points.Contains(pointstring))
+                {
+                    EndPointIndex.Add(Points.IndexOf(pointstring));
+                }
+                else
+                {
+                    Points.Add(pointstring);
+                    EndPointIndex.Add(Points.IndexOf(pointstring));
+                }
+            }
+
+            json += ",[[";
+            for (int i = 0; i < Points.Count; i++)
+            {
+                json += Points[i] + ",";
+            }
+            json = json.Trim(',') + "]]";
+
+
+            //Add Bar Message
+
+            json += ",[";
+
+            for (int i = 0; i < barList.Count; i++)
+            {
+                json += "[[" + SectionProperties.IndexOf(barList[i].SectionProperty.Name.Replace(" ", string.Empty)) + "],[" + ColorIndex[i] + "],[" + StartPointIndex[i] + "," + EndPointIndex[i] + "]],";
+            }
+            json = json.Trim(',') + "]";
+            DA.SetData(0, json);
 
         }
     }
