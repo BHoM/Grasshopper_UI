2e72778ea2799e2792649e8b300e1939c5284f8a
adecler <arnaud.declercq@burohappold.com>
2017-02-03 09:23:01 +0000

    Adding the MongoServer components

---
+++ Mongo_Alligator/DeleteDatabase.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grasshopper.Kernel;
+using GHE = Grasshopper_Engine;
+using MA = Mongo_Adapter;
+
+namespace Alligator.Mongo
+{
+    public class DeleteDatabase : GH_Component
+    {
+        public DeleteDatabase() : base("DeleteDatabase", "DeleteDatabase", "Delete teh specific database from the Mongo server.", "Alligator", "Mongo") { }
+
+        public override Guid ComponentGuid
+        {
+            get
+            {
+                return new Guid("1F07F868-42C1-4531-8902-AD446038752C");
+            }
+        }
+
+        public override GH_Exposure Exposure
+        {
+            get
+            {
+                return GH_Exposure.secondary;
+            }
+        }
+
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("server", "server", "Mongo server", GH_ParamAccess.item);
+            pManager.AddTextParameter("database", "database", "database name", GH_ParamAccess.item);
+            pManager.AddBooleanParameter("activate", "activate", "trigger the command", GH_ParamAccess.item);
+        }
+
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddBooleanParameter("success", "success", "success", GH_ParamAccess.item);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            MA.MongoServer server = GHE.DataUtils.GetGenericData<MA.MongoServer>(DA, 0);
+            string database = GHE.DataUtils.GetData<string>(DA, 1);
+            bool activate = GHE.DataUtils.GetData<bool>(DA, 2);
+
+            if (!activate)
+            {
+                DA.SetData(0, false);
+            }
+            else
+            {
+                DA.SetData(0, server.DeleteDatabase(database));
+            } 
+
+            
+        }
+    }
+}
---
+++ Mongo_Alligator/GetDatabases.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grasshopper.Kernel;
+using GHE = Grasshopper_Engine;
+using MA = Mongo_Adapter;
+
+namespace Alligator.Mongo
+{
+    public class GetDatabases : GH_Component
+    {
+        public GetDatabases() : base("GetDatabases", "GetDatabases", "Get the list of databases curretly available in the Mongo server.", "Alligator", "Mongo") { }
+
+        public override Guid ComponentGuid
+        {
+            get
+            {
+                return new Guid("551E958A-0EBC-4833-ABED-47ED8E9B7A1B");
+            }
+        }
+
+        public override GH_Exposure Exposure
+        {
+            get
+            {
+                return GH_Exposure.secondary;
+            }
+        }
+
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("server", "server", "Mongo server", GH_ParamAccess.item);
+        }
+
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("databases", "databases", "list of databases names", GH_ParamAccess.list);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            MA.MongoServer server = GHE.DataUtils.GetGenericData<MA.MongoServer>(DA, 0);
+
+            DA.SetDataList(0, server.GetAllDatabases());
+        }
+    }
+}
