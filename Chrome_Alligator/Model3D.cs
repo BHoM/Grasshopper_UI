2cf8ec7314d817b6b6e048c41d630ceaf7bb5a2c
adecler <arnaud.declercq@burohappold.com>
2017-05-31 15:01:20 +0100

    adding model3D to the views available to Chrome adapter

--- Chrome_Alligator/Model3D.cs
+++ Chrome_Alligator/Model3D.cs
@@ -9,15 +9,15 @@ using CA = Chrome_Adapter;
 
 namespace Alligator.Mongo
 {
-    public class VideoConfig : GH_Component
+    public class Model3D : GH_Component
     {
-        public VideoConfig() : base("Video", "Video", "Define the configuration of the videos pushed to Chrome.", "Alligator", "Chrome") { }
+        public Model3D() : base("Model3D", "Model3D", "Define the configuration of the 3d models pushed to Chrome.", "Alligator", "Chrome") { }
 
         public override Guid ComponentGuid
         {
             get
             {
-                return new Guid("823ED29C-ADBB-4CCC-AF31-1ED71BE39F8E");
+                return new Guid("ACBF7646-24C4-4ECE-9245-6D898976F68B");
             }
         }
 
@@ -33,8 +33,8 @@ namespace Alligator.Mongo
         {
             pManager.AddTextParameter("parent", "parent", "Define the parent to this component", GH_ParamAccess.item, "body");
             pManager.AddTextParameter("dim", "dim", "Dimension where the video is stored. Leave blank if the video is the data itself.", GH_ParamAccess.item, "");
-            pManager.AddBooleanParameter("controls", "controls", "Show video controls", GH_ParamAccess.item, true);
-            pManager.AddBooleanParameter("autoplay", "autoplay", "Play the video automatically.", GH_ParamAccess.item, false);
+            pManager.AddTextParameter("camPos", "camPos", "Define the position of the camera. Format: [x,y,z]", GH_ParamAccess.item, "");
+            pManager.AddTextParameter("camTarget", "camTarget", "Define the target of the camera. Format: [x,y,z]", GH_ParamAccess.item, "");
         }
 
         protected override void RegisterOutputParams(GH_OutputParamManager pManager)
@@ -44,22 +44,26 @@ namespace Alligator.Mongo
 
         protected override void SolveInstance(IGH_DataAccess DA)
         {
-            string parent = "", dim = "";
+            string parent = "", dim = "", pos = "", target = "";
             bool controls = true, autoplay = false;
             DA.GetData<string>(0, ref parent);
             DA.GetData<string>(1, ref dim);
-            DA.GetData<bool>(2, ref controls);
-            DA.GetData<bool>(3, ref autoplay);
+            DA.GetData<string>(2, ref pos);
+            DA.GetData<string>(3, ref target);
 
             List<string> config = new List<string>();
 
-            config.Add("type: video");
+            config.Add("type: model3d");
             config.Add("parent: " + parent);
-            config.Add("controls: " + controls.ToString().ToLower());
-            config.Add("autoplay: " + autoplay.ToString().ToLower());
 
             if (dim.Length > 0)
                 config.Add("dim: " + dim);
+
+            if (pos.Length > 0)
+                config.Add("camPos: " + pos);
+
+            if (target.Length > 0)
+                config.Add("camTarget: " + target);
 
             DA.SetDataList(0, config);
         }
