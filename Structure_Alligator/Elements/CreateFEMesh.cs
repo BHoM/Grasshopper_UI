dcf5dba6f9ed3ca66aa9c69fc1ff0e17a3c0c0ba
Puria Safari Hesari <puria.safarihesari@burohappold.com>
2017-09-28 10:13:34 +0100

    putting into BHoM2.0_Consolidate

--- Structure_Alligator/Elements/CreateFEMesh.cs
+++ Structure_Alligator/Elements/CreateFEMesh.cs
@@ -1,99 +1,99 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Grasshopper.Kernel;
-using BH.oM.Structural.Elements;
-using BH.oM.Structural.Properties;
-using Rhino.Geometry;
-using GHE = BH.Engine.Grasshopper;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Grasshopper.Kernel;
+//using BH.oM.Structural.Elements;
+//using BH.oM.Structural.Properties;
+//using Rhino.Geometry;
+//using GHE = BH.Engine.Grasshopper;
 
-namespace BH.UI.Alligator.Structural.Elements
-{
-    public class CreateFEMesh : GH_Component
-    {
-        public CreateFEMesh() : base("Create FEMesh", "FEMesh", "Create a BH FEmesh object", "Structure", "Elements") { }
+//namespace BH.UI.Alligator.Structural.Elements
+//{
+//    public class CreateFEMesh : GH_Component
+//    {
+//        public CreateFEMesh() : base("Create FEMesh", "FEMesh", "Create a BH FEmesh object", "Structure", "Elements") { }
 
-        /// <summary> Icon (24x24 pixels)</summary>
-        protected override System.Drawing.Bitmap Internal_Icon_24x24
-        {
-            get { return Alligator.Structural.Properties.Resources.BHoM_Mesh; }
-        }
+//        /// <summary> Icon (24x24 pixels)</summary>
+//        protected override System.Drawing.Bitmap Internal_Icon_24x24
+//        {
+//            get { return Alligator.Structural.Properties.Resources.BHoM_Mesh; }
+//        }
 
-        public override Guid ComponentGuid
-        {
-            get
-            {
-                return new Guid("6F367478-D7A1-4AC7-825E-DAFEB56B77BA");
-            }
-        }
+//        public override Guid ComponentGuid
+//        {
+//            get
+//            {
+//                return new Guid("6F367478-D7A1-4AC7-825E-DAFEB56B77BA");
+//            }
+//        }
 
-        protected override void RegisterInputParams(GH_InputParamManager pManager)
-        {
-            pManager.AddMeshParameter("Mesh", "M", "Rhinomesh to convert", GH_ParamAccess.item);
-            pManager.AddGenericParameter("Panel Property", "P", "Panelproperty to apply to the mesh", GH_ParamAccess.item);
-            pManager.AddTextParameter("Name", "N", "Name of the mesh", GH_ParamAccess.item);
-            pManager.AddGenericParameter("Custom Data", "CD", "Customd ata for the mesh", GH_ParamAccess.item);
+//        protected override void RegisterInputParams(GH_InputParamManager pManager)
+//        {
+//            pManager.AddMeshParameter("Mesh", "M", "Rhinomesh to convert", GH_ParamAccess.item);
+//            pManager.AddGenericParameter("Panel Property", "P", "Panelproperty to apply to the mesh", GH_ParamAccess.item);
+//            pManager.AddTextParameter("Name", "N", "Name of the mesh", GH_ParamAccess.item);
+//            pManager.AddGenericParameter("Custom Data", "CD", "Customd ata for the mesh", GH_ParamAccess.item);
 
-            pManager[2].Optional = true;
-            pManager[3].Optional = true;
-        }
+//            pManager[2].Optional = true;
+//            pManager[3].Optional = true;
+//        }
 
-        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
-        {
-            pManager.AddGenericParameter("FE-Mesh", "FEM", "The constructed FE-Mesh", GH_ParamAccess.item);
-        }
+//        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+//        {
+//            pManager.AddGenericParameter("FE-Mesh", "FEM", "The constructed FE-Mesh", GH_ParamAccess.item);
+//        }
 
-        protected override void SolveInstance(IGH_DataAccess DA)
-        {
-            Mesh mesh = GHE.DataUtils.GetData<Mesh>(DA, 0);
-            PanelProperty prop = GHE.DataUtils.GetGenericData<PanelProperty>(DA, 1);
+//        protected override void SolveInstance(IGH_DataAccess DA)
+//        {
+//            Mesh mesh = GHE.DataUtils.GetData<Mesh>(DA, 0);
+//            PanelProperty prop = GHE.DataUtils.GetGenericData<PanelProperty>(DA, 1);
 
-            if (mesh == null || prop == null)
-                return;
+//            if (mesh == null || prop == null)
+//                return;
 
-            FEMesh feMesh = new FEMesh();
+//            FEMesh feMesh = new FEMesh();
 
-            feMesh.PanelProperty = prop;
+//            feMesh.PanelProperty = prop;
 
-            for (int i = 0; i < mesh.Vertices.Count; i++)
-            {
-                feMesh.Nodes.Add(new Node(GHE.GeometryUtils.Convert(mesh.Vertices[i])));
-            }
+//            for (int i = 0; i < mesh.Vertices.Count; i++)
+//            {
+//                feMesh.Nodes.Add(new Node(GHE.GeometryUtils.Convert(mesh.Vertices[i])));
+//            }
 
-            foreach (MeshFace face in mesh.Faces)
-            {
-                FEFace feFace = new FEFace();
+//            foreach (MeshFace face in mesh.Faces)
+//            {
+//                FEFace feFace = new FEFace();
 
-                feFace.NodeIndices.Add(face.A);
-                feFace.NodeIndices.Add(face.B);
-                feFace.NodeIndices.Add(face.C);
+//                feFace.NodeIndices.Add(face.A);
+//                feFace.NodeIndices.Add(face.B);
+//                feFace.NodeIndices.Add(face.C);
 
-                if (face.IsQuad)
-                    feFace.NodeIndices.Add(face.D);
+//                if (face.IsQuad)
+//                    feFace.NodeIndices.Add(face.D);
 
-                feMesh.Faces.Add(feFace);
-            }
+//                feMesh.Faces.Add(feFace);
+//            }
 
-            string name = null;
+//            string name = null;
 
-            if (DA.GetData(2, ref name))
-            {
-                feMesh.Name = name;
-            }
+//            if (DA.GetData(2, ref name))
+//            {
+//                feMesh.Name = name;
+//            }
 
-            Dictionary<string, object> customData = GHE.DataUtils.GetGenericData<Dictionary<string, object>>(DA, 3);
+//            Dictionary<string, object> customData = GHE.DataUtils.GetGenericData<Dictionary<string, object>>(DA, 3);
 
-            if (customData != null)
-            {
-                foreach (KeyValuePair<string,object> kvp in customData)
-                {
-                    feMesh.CustomData[kvp.Key] = kvp.Value;
-                }
-            }
+//            if (customData != null)
+//            {
+//                foreach (KeyValuePair<string,object> kvp in customData)
+//                {
+//                    feMesh.CustomData[kvp.Key] = kvp.Value;
+//                }
+//            }
 
-            DA.SetData(0, feMesh);
-        }
-    }
-}
+//            DA.SetData(0, feMesh);
+//        }
+//    }
+//}
--- Structure_Alligator/Elements/ImportBar.cs
+++ Structure_Alligator/Elements/ImportBar.cs
@@ -1,93 +1,93 @@
-﻿using BH.oM.Structural;
-using System;
-using Grasshopper.Kernel;
-using System.Collections.Generic;
-using GHE = BH.Engine.Grasshopper;
-using BH.UI.Alligator.Components;
-using BHE = BH.oM.Structural.Elements;
-using BHI = BH.oM.Structural.Interface;
-using BHP = BH.oM.Structural.Properties;
-using BHG = BH.oM.Geometry;
-using System.Windows.Forms;
-using R = Rhino.Geometry;
-using Grasshopper;
-using GHKT = Grasshopper.Kernel.Types;
-using BH.Engine.Grasshopper.Components;
-using Grasshopper.Kernel.Data;
-using BH.Engine.Structure;
-using BH.Adapter;
+﻿//using BH.oM.Structural;
+//using System;
+//using Grasshopper.Kernel;
+//using System.Collections.Generic;
+//using GHE = BH.Engine.Grasshopper;
+//using BH.UI.Alligator.Components;
+//using BHE = BH.oM.Structural.Elements;
+//using BHI = BH.oM.Structural.Interface;
+//using BHP = BH.oM.Structural.Properties;
+//using BHG = BH.oM.Geometry;
+//using System.Windows.Forms;
+//using R = Rhino.Geometry;
+//using Grasshopper;
+//using GHKT = Grasshopper.Kernel.Types;
+//using BH.Engine.Grasshopper.Components;
+//using Grasshopper.Kernel.Data;
+//using BH.Engine.Structure;
+//using BH.Adapter;
 
 
-namespace BH.UI.Alligator.Structural.Elements
-{
-    public class ImportBar : ImportComponent<BHE.Bar>
-    {       
-        public ImportBar() : base("Import Bar", "GetBar", "Get the geometry and properties of a Bar", "Structure", "Elements")
-        {
+//namespace BH.UI.Alligator.Structural.Elements
+//{
+//    public class ImportBar : ImportComponent<BHE.Bar>
+//    {       
+//        public ImportBar() : base("Import Bar", "GetBar", "Get the geometry and properties of a Bar", "Structure", "Elements")
+//        {
            
-        }
+//        }
 
-        public override GH_Exposure Exposure
-        {
-            get
-            {
-                return GH_Exposure.tertiary;
-            }
-        }
+//        public override GH_Exposure Exposure
+//        {
+//            get
+//            {
+//                return GH_Exposure.tertiary;
+//            }
+//        }
 
-        protected override void SolveInstance(IGH_DataAccess DA)
-        {
-            if (GHE.DataUtils.Run(DA, 2))
-            {
-                IndexAdapter app = GHE.DataUtils.GetGenericData<IndexAdapter>(DA, 0);
-                if (app != null)
-                {
-                    List<string> ids = null;
-                    List<BHE.Bar> bars = null;
-                    DataTree<R.Line> curves = new DataTree<R.Line>();
-                    if (m_Selection == BHI.ObjectSelection.FromInput)
-                        ids = GHE.DataUtils.GetDataList<string>(DA, 1);
+//        protected override void SolveInstance(IGH_DataAccess DA)
+//        {
+//            if (GHE.DataUtils.Run(DA, 2))
+//            {
+//                IndexAdapter app = GHE.DataUtils.GetGenericData<IndexAdapter>(DA, 0);
+//                if (app != null)
+//                {
+//                    List<string> ids = null;
+//                    List<BHE.Bar> bars = null;
+//                    DataTree<R.Line> curves = new DataTree<R.Line>();
+//                    if (m_Selection == BHI.ObjectSelection.FromInput)
+//                        ids = GHE.DataUtils.GetDataList<string>(DA, 1);
 
-                    //app.Selection = m_Selection;
-                    ids = app.GetBars(out bars, ids);
-                    app.Pull()
+//                    //app.Selection = m_Selection;
+//                    ids = app.GetBars(out bars, ids);
+//                    app.Pull();
 
-                    for (int i = 0; i < bars.Count; i++)
-                    {
-                        curves.Add(GHE.GeometryUtils.Convert(bars[i].GetCentreline()));
-                    }
+//                    for (int i = 0; i < bars.Count; i++)
+//                    {
+//                        curves.Add(GHE.GeometryUtils.Convert(bars[i].GetCentreline()));
+//                    }
 
-                    DA.SetDataList(0, ids);
-                    DA.SetDataList(1, bars);
-                    DA.SetDataTree(2, curves);
-                }
-            }
-        }
+//                    DA.SetDataList(0, ids);
+//                    DA.SetDataList(1, bars);
+//                    DA.SetDataTree(2, curves);
+//                }
+//            }
+//        }
 
-        public override List<BHE.Bar> GetObjects(BHI.IElementAdapter app, List<string> objectIds, out IGH_DataTree geom, out List<string> outIds)
-        {
-            List<BHE.Bar> result = null;
-            outIds = app.GetBars(out result, objectIds);
+//        public override List<BHE.Bar> GetObjects(BHI.IElementAdapter app, List<string> objectIds, out IGH_DataTree geom, out List<string> outIds)
+//        {
+//            List<BHE.Bar> result = null;
+//            outIds = app.GetBars(out result, objectIds);
 
-            DataTree<R.Line> curves = new DataTree<R.Line>();
-            for (int i = 0; i < result.Count; i++)
-            {
-                curves.Add(GHE.GeometryUtils.Convert(result[i].GetCentreline()));
-            }
-            geom = curves;
-            return result;
-        }
+//            DataTree<R.Line> curves = new DataTree<R.Line>();
+//            for (int i = 0; i < result.Count; i++)
+//            {
+//                curves.Add(GHE.GeometryUtils.Convert(result[i].GetCentreline()));
+//            }
+//            geom = curves;
+//            return result;
+//        }
 
-        public override Guid ComponentGuid
-        {
-            get { return new Guid("1320dc1b-87b3-491a-93fa-1495315aa5a2"); }
-        }
+//        public override Guid ComponentGuid
+//        {
+//            get { return new Guid("1320dc1b-87b3-491a-93fa-1495315aa5a2"); }
+//        }
 
-        /// <summary> Icon (24x24 pixels)</summary>
-        protected override System.Drawing.Bitmap Internal_Icon_24x24
-        {
-            get { return Alligator.Structural.Properties.Resources.BHoM_Bar_Import; }
-        }
-    }
+//        /// <summary> Icon (24x24 pixels)</summary>
+//        protected override System.Drawing.Bitmap Internal_Icon_24x24
+//        {
+//            get { return Alligator.Structural.Properties.Resources.BHoM_Bar_Import; }
+//        }
+//    }
 
-}
+//}
