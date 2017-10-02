400c6fdd0d42367f9249924dfb76a5480c14e53d
Eduardo Pignatelli <eduardo.pignatelli@burohappold.com>
2017-10-02 12:33:59 +0100

    Reinstated CrowdAnalysis from hidden files

---
+++ MachineLearning_Alligator/VideoAnalytics/StadiaCrowdAnalysis.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grasshopper.Kernel;
+using GHE = Grasshopper_Engine;
+
+using StadiaCrowdAnalysis_Engine;
+
+namespace StadiaCrowdAnalytics_Alligator
+{
+    public class StadiaCrowdAnalysis
+    {
+    }
+}
