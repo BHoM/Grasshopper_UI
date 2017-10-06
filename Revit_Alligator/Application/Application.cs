c6860f62484085efd60338699681475cfbff200d
Rob May <rob.may@burohappold.com>
2017-10-06 11:29:11 +0100

    NugetPackageUpdates
    for quick commit @epignatelli

--- Revit_Alligator/Application/Application.cs
+++ Revit_Alligator/Application/Application.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using BHoM.Structural;
+using BH.oM.Structural;
 using System.IO;
 using System.Drawing;
 
