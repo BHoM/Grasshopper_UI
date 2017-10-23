e769c1f720c956c27bccd3fd27d4a77b3a2a1b98
adecler <arnaud.declercq@burohappold.com>
2017-10-23 13:49:11 +0800

    Fixing CRUD Components

--- Adapter_Alligator/CRUD/Push.cs
+++ Adapter_Alligator/CRUD/Push.cs
@@ -28,6 +28,7 @@ namespace BH.UI.Alligator.Adapter
             pManager.AddTextParameter("Tag", "Tag", "Tag to apply to the objects being pushed", GH_ParamAccess.item);
             pManager.AddGenericParameter("Config", "Config", "Delete config", GH_ParamAccess.item);
             pManager.AddBooleanParameter("Active", "Active", "Execute the push", GH_ParamAccess.item);
+            Params.Input[3].Optional = true;
         }
 
         protected override void RegisterOutputParams(GH_OutputParamManager pManager)
@@ -37,11 +38,11 @@ namespace BH.UI.Alligator.Adapter
 
         protected override void SolveInstance(IGH_DataAccess DA)
         {
-            BHoMAdapter adapter = null; DA.BH_GetData(0, adapter);
-            List<BHoMObject> objects = new List<BHoMObject>(); DA.BH_GetDataList(1, objects);
-            string tag = ""; DA.BH_GetData(2, tag);
-            Dictionary<string, string> config = null; DA.BH_GetData(3, config);
-            bool active = false; DA.BH_GetData(4, active);
+            BHoMAdapter adapter = null; DA.GetData(0, ref adapter);
+            List<BHoMObject> objects = new List<BHoMObject>(); DA.GetDataList(1, objects);
+            string tag = ""; DA.GetData(2, ref tag);
+            Dictionary<string, string> config = null; DA.GetData(3, ref config);
+            bool active = false; DA.GetData(4, ref active);
 
             if (!active) return;
 
