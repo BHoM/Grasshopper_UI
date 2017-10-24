e46a27b6f55e6bcf96c5904b6aa72158ad6a383e
adecler <arnaud.declercq@burohappold.com>
2017-10-24 13:32:52 +0800

    Align with the GetGeometry definition in the Engine

--- Alligator/NonComponents/BH_Types/DrawBHoMObjects.cs
+++ Alligator/NonComponents/BH_Types/DrawBHoMObjects.cs
@@ -15,48 +15,62 @@ namespace BH.UI.Alligator
         public static void IRenderBHoMGeometry(this BHG.IBHoMGeometry geometry, GH_PreviewWireArgs args)
         {
             args.Pipeline.ZBiasMode = 0;
-            Color bhColour = new Color();   // To automatically keep the change of color when selected a BHcolour is set as dependent from GH colour
-            int R = args.Color.R - 59;      // Difference to BuroHappold Green
-            int G = args.Color.G + 168;     // Difference to BuroHappold Green
-            int B = args.Color.B;           // Difference to BuroHappold Green
-            bhColour = Color.FromArgb(100, R < 255 && R > 0 ? R : 0, G < 255 && G > 0 ? G : 255, B);
+            Color bhColour = GetBHColor(args.Color);
 
-            RenderBHoMGeometry(geometry as dynamic, args, bhColour);
+            RenderBHoMGeometry(geometry as dynamic, args.Pipeline, bhColour);
         }
-        public static void RenderBHoMGeometry(BHG.Point point, GH_PreviewWireArgs args, Color bhColour)
+
+        public static void IRenderBHoMGeometry(this BHG.IBHoMGeometry geometry, IGH_PreviewArgs args)
         {
-            args.Pipeline.DrawPoint(point.ToRhino(), Rhino.Display.PointStyle.Simple, 3, bhColour);
+            args.Display.ZBiasMode = 0;
+            Color bhColour = GetBHColor(args.WireColour);
+
+            RenderBHoMGeometry(geometry as dynamic, args.Display, bhColour);
         }
-        public static void RenderBHoMGeometry(BHG.Vector vector, GH_PreviewWireArgs args, Color bhColour)
+
+        public static void RenderBHoMGeometry(BHG.Point point, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
+        {
+            pipeline.DrawPoint(point.ToRhino(), Rhino.Display.PointStyle.Simple, 3, bhColour);
+        }
+        public static void RenderBHoMGeometry(BHG.Vector vector, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
         {
             //args.Pipeline.DrawLineArrow(vector.ToRhino(), args.Color, args.Thickness, args.Thickness);
         }
-        public static void RenderBHoMGeometry(BHG.Line line, GH_PreviewWireArgs args, Color bhColour)
+        public static void RenderBHoMGeometry(BHG.Line line, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
         {
-            args.Pipeline.DrawLine(line.ToRhino(), bhColour);
+            pipeline.DrawLine(line.ToRhino(), bhColour);
         }
-        public static void RenderBHoMGeometry(BHG.Circle circle, GH_PreviewWireArgs args, Color bhColour)
+        public static void RenderBHoMGeometry(BHG.Circle circle, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
         {
-            args.Pipeline.DrawCircle(circle.ToRhino(), bhColour);
+            pipeline.DrawCircle(circle.ToRhino(), bhColour);
         }
-        public static void RenderBHoMGeometry(BHG.Polyline polyline, GH_PreviewWireArgs args, Color bhColour)
+        public static void RenderBHoMGeometry(BHG.Polyline polyline, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
         {
             List<BH.oM.Geometry.Point> bhomPoints = polyline.ControlPoints;
             IEnumerable<Rhino.Geometry.Point3d> rhinoPoints = bhomPoints.Select(x => (x).ToRhino());
-            args.Pipeline.DrawPolyline(rhinoPoints, bhColour);
+            pipeline.DrawPolyline(rhinoPoints, bhColour);
         }
-        public static void RenderBHoMGeometry(BHG.NurbCurve curve, GH_PreviewWireArgs args, Color bhColour)
+        public static void RenderBHoMGeometry(BHG.NurbCurve curve, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
         {
         }
-        public static void RenderBHoMGeometry(BHG.Plane plane, GH_PreviewWireArgs args, Color bhColour)
+        public static void RenderBHoMGeometry(BHG.Plane plane, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
         {
-            args.Pipeline.DrawConstructionPlane(new Rhino.DocObjects.ConstructionPlane() { Plane = (plane.ToRhino()) });
+            pipeline.DrawConstructionPlane(new Rhino.DocObjects.ConstructionPlane() { Plane = (plane.ToRhino()) });
         }
-        public static void RenderBHoMGeometry(BHG.NurbSurface surface, GH_PreviewWireArgs args, Color bhColour)
+        public static void RenderBHoMGeometry(BHG.NurbSurface surface, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
         {
         }
-        public static void RenderBHoMGeometry(BHG.Mesh mesh, GH_PreviewWireArgs args, Color bhColour)
+        public static void RenderBHoMGeometry(BHG.Mesh mesh, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
         {
+        }
+
+
+        private static Color GetBHColor(Color ghColor)
+        {
+            int R = ghColor.R - 59;      // Difference to BuroHappold Green
+            int G = ghColor.G + 168;     // Difference to BuroHappold Green
+            int B = ghColor.B;           // Difference to BuroHappold Green
+            return Color.FromArgb(100, R < 255 && R > 0 ? R : 0, G < 255 && G > 0 ? G : 255, B);
         }
     }
 }
