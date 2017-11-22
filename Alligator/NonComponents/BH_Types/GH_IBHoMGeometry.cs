a8ca8fba97d75e46bcec3643fbb60d7613e11da4
epignatel <eduardo.pignatelli@burohappold.com>
2017-11-22 14:10:19 +0000

    Updated previews + Added RenderBHoMObject and Render BHoMGeometry + Fixed casting

--- Alligator/NonComponents/BH_Types/GH_IBHoMGeometry.cs
+++ Alligator/NonComponents/BH_Types/GH_IBHoMGeometry.cs
@@ -11,23 +11,30 @@ using BH.Engine.Base;
 using Grasshopper.Kernel.Parameters;
 using BH.Adapter.Rhinoceros;
 using BH.UI.Alligator.Base;
+using Rhino;
+using Rhino.DocObjects;
 
 namespace BH.UI.Alligator
 {
-    public class GH_IBHoMGeometry : GH_GeometricGoo<IBHoMGeometry>, IGH_PreviewData
+    public class GH_IBHoMGeometry : GH_GeometricGoo<IBHoMGeometry>, IGH_PreviewData, IGH_BakeAwareData
     {
-        public GH_IBHoMGeometry()
+        /***************************************************/
+        /**** Properties Override                       ****/
+        /***************************************************/
+
+        public override string TypeName
         {
-            this.Value = null;
+            get { return ("IBHoMGeometry"); }
         }
-        public GH_IBHoMGeometry(IBHoMGeometry bh)
+
+        /***************************************************/
+
+        public override string TypeDescription
         {
-            this.Value = bh;
+            get { return ("Contains a generic IBHoMGeometry"); }
         }
-        public override IGH_GeometricGoo DuplicateGeometry()
-        {
-            return new GH_IBHoMGeometry { Value = Value.IGetClone() };
-        }
+
+        /***************************************************/
 
         public override bool IsValid
         {
@@ -37,97 +44,123 @@ namespace BH.UI.Alligator
                 return true;
             }
         }
-        public override string ToString()
-        {
-            if (Value == null)
-                return "Null IBHoMGeometry";
-            else if (typeof(BH.oM.Geometry.Point).IsAssignableFrom(Value.GetType()))
-            {
-                return (Value.GetType().ToString() + " {" + ((BH.oM.Geometry.Point)Value).X + ", " +
-                                                            ((BH.oM.Geometry.Point)Value).Y + ", " +
-                                                            ((BH.oM.Geometry.Point)Value).Z + "}");
-            }
-            else
-                return Value.ToString();
-        }
-        public override string TypeName
-        {
-            get { return ("IBHoMGeometry"); }
-        }
-        public override string TypeDescription
-        {
-            get { return ("Defines a generic IBHoMGeometry"); }
-        }
+
+        /***************************************************/
 
         public override Rhino.Geometry.BoundingBox Boundingbox
         {
             get
             {
                 if (Value == null) { return Rhino.Geometry.BoundingBox.Empty; }
-                if (Value == null) { return Rhino.Geometry.BoundingBox.Empty; }
-                BH.oM.Geometry.BoundingBox bb = Value.IGetBounds();
-                return new Rhino.Geometry.BoundingBox(bb.Min.X, bb.Min.Y, bb.Min.Z, bb.Max.X, bb.Max.Y, bb.Max.Z);
+                return Value.IGetBounds().ToRhino();
             }
         }
+
+        /***************************************************/
+
+        public Rhino.Geometry.BoundingBox ClippingBox
+        {
+            get { return Boundingbox; }
+        }
+
+
+        /***************************************************/
+        /**** Constructors                              ****/
+        /***************************************************/
+
+        public GH_IBHoMGeometry()
+        {
+            this.Value = null;
+        }
+
+        /***************************************************/
+
+        public GH_IBHoMGeometry(IBHoMGeometry bh)
+        {
+            this.Value = bh;
+        }
+
+
+        /***************************************************/
+        /**** Public Methods Override                   ****/
+        /***************************************************/
+
+        public override IGH_GeometricGoo DuplicateGeometry()
+        {
+            return new GH_IBHoMGeometry { Value = Value.IGetClone() };
+        }
+
+        /***************************************************/
+
+        public override string ToString()
+        {
+            string type = Value.GetType().ToString();
+            if (Value == null) { return "null IBHoMGeometry"; }
+            else if (typeof(BH.oM.Geometry.Point).IsAssignableFrom(Value.GetType()))
+            {
+                BH.oM.Geometry.Point pt = (BH.oM.Geometry.Point)Value;
+                return (Value.GetType().ToString() + " {" + pt.X + ", " + pt.Y + ", " + pt.Z + "}");
+            }
+            else if (typeof(BH.oM.Geometry.Vector).IsAssignableFrom(Value.GetType()))
+            {
+                BH.oM.Geometry.Vector vec = (BH.oM.Geometry.Vector)Value;
+                return (Value.GetType().ToString() + " {" + vec.X + ", " + vec.Y + ", " + vec.Z + "}");
+            }
+            else
+                return Value.ToString();
+        }
+
+        /***************************************************/
+
         public override Rhino.Geometry.BoundingBox GetBoundingBox(Rhino.Geometry.Transform xform)
         {
             if (Value == null) { return Rhino.Geometry.BoundingBox.Empty; }
-            if (Value == null) { return Rhino.Geometry.BoundingBox.Empty; }
-            BH.oM.Geometry.BoundingBox bb = Value.IGetBounds();
-            return new Rhino.Geometry.BoundingBox(bb.Min.X, bb.Min.Y, bb.Min.Z, bb.Max.X, bb.Max.Y, bb.Max.Z);
+            return Value.IGetBounds().ToRhino();
         }
+
+
+        /***************************************************/
+        /**** Automatic casting methods                 ****/
+        /***************************************************/
 
         public override bool CastFrom(object source)
         {
             if (source == null) { return false; }
-            if (typeof(IBHoMGeometry).IsAssignableFrom(source.GetType()))
-            {
+
+            else if (typeof(IBHoMGeometry).IsAssignableFrom(source.GetType()))
                 this.Value = (IBHoMGeometry)source;
-                return true;
-            }
-            if (typeof(Rhino.Geometry.GeometryBase).IsAssignableFrom(source.GetType()))
-            {
+            else if (typeof(Rhino.Geometry.GeometryBase).IsAssignableFrom(source.GetType()))
                 this.Value = ((Rhino.Geometry.GeometryBase)source).IToBHoM();
-                return true;
-            }
-            if (typeof(GH_Point).IsAssignableFrom(source.GetType()))
-            {
+
+            else if (typeof(GH_Point).IsAssignableFrom(source.GetType()))
                 Value = (((GH_Point)source).Value).ToBHoM();
-            }
             else if (typeof(GH_Vector).IsAssignableFrom(source.GetType()))
-            {
                 Value = (((GH_Vector)source).Value).ToBHoM();
-            }
-            else if (typeof(GH_Line).IsAssignableFrom(source.GetType()))
-            {
-                Value = (((GH_Line)source).Value).ToBHoM();
-            }
-            else if (typeof(GH_Circle).IsAssignableFrom(source.GetType()))
-            {
-                Value = (((GH_Circle)source).Value).ToBHoM();
-            }
-            else if (typeof(GH_Curve).IsAssignableFrom(source.GetType()))
-            {
-                Value = (((GH_Curve)source).Value).ToBHoM();
-            }
             else if (typeof(GH_Plane).IsAssignableFrom(source.GetType()))
-            {
                 Value = (((GH_Plane)source).Value).ToBHoM();
-            }
+
+            else if (typeof(GH_Arc).IsAssignableFrom(source.GetType()))
+                Value = (((GH_Arc)source).Value).ToBHoM();
+            else if (typeof(GH_Circle).IsAssignableFrom(source.GetType()))
+                Value = (((GH_Circle)source).Value).ToBHoM();
+            else if (typeof(GH_Curve).IsAssignableFrom(source.GetType()))
+                Value = (((GH_Curve)source).Value).ToBHoM();
+            else if (typeof(GH_Line).IsAssignableFrom(source.GetType()))
+                Value = (((GH_Line)source).Value).ToBHoM();
+
+            else if (typeof(GH_Box).IsAssignableFrom(source.GetType()))
+                Value = (((GH_Box)source).Value).ToBHoM();
             else if (typeof(GH_Surface).IsAssignableFrom(source.GetType()))
-            {
                 Value = (((GH_Surface)source).Value).ToBHoM();
-            }
             else if (typeof(GH_Brep).IsAssignableFrom(source.GetType()))
-            {
                 Value = (((GH_Brep)source).Value).ToBHoM();
-            }
             else if (typeof(GH_Mesh).IsAssignableFrom(source.GetType()))
-            {
                 Value = (((GH_Mesh)source).Value).ToBHoM();
-            }
             return true;
         }
+
+        /***************************************************/
+
         public override bool CastTo<Q>(ref Q target)
         {
             object ptr = this.Value;
@@ -135,39 +168,54 @@ namespace BH.UI.Alligator
             return true;
         }
 
+
+        /***************************************************/
+        /**** Transformation methods                    ****/
+        /***************************************************/
+
         public override IGH_GeometricGoo Transform(Rhino.Geometry.Transform xform)
         {
-            // This does not make any sense, so I will just do a validity check
             if (Value == null) { return null; }
-            return new GH_IBHoMGeometry();
-        }
-        public override IGH_GeometricGoo Morph(Rhino.Geometry.SpaceMorph xmorph)
-        {
-            // This does not make any sense, so I will just do a validity check
-            if (Value == null) { return null; }
-            return new GH_IBHoMGeometry();
+            return this;
         }
 
-        public Rhino.Geometry.BoundingBox ClippingBox
+        /***************************************************/
+
+        public override IGH_GeometricGoo Morph(Rhino.Geometry.SpaceMorph xmorph)
         {
-            get { return Boundingbox; }
+            if (Value == null) { return null; }
+            return this;
         }
+
+
+        /***************************************************/
+        /**** IGH_PreviewData methods                   ****/
+        /***************************************************/
+
         public void DrawViewportMeshes(GH_PreviewMeshArgs args)
         {
+            if (Value == null) { return; }
             if (typeof(BH.oM.Geometry.Mesh).IsAssignableFrom(Value.GetType()))
-            {
-                args.Pipeline.DrawMeshWires(((BH.oM.Geometry.Mesh)Value).ToRhino(), args.Material.Diffuse);
-            }
-            if (typeof(BH.oM.Geometry.Mesh).IsAssignableFrom(Value.GetType()))
-            {
-                args.Pipeline.DrawMeshShaded(((BH.oM.Geometry.Mesh)Value).ToRhino(), args.Material);
-            }
+                Render.RenderBHoMGeometry((BH.oM.Geometry.Mesh)Value, args);
         }
+
+        /***************************************************/
+
         public void DrawViewportWires(GH_PreviewWireArgs args)
         {
             if (Value == null) { return; }
             Render.IRenderBHoMGeometry(Value as dynamic, args);
         }
 
+
+        /***************************************************/
+        /**** IGH_BakeAwareData methods                 ****/
+        /***************************************************/
+
+        public bool BakeGeometry(RhinoDoc doc, ObjectAttributes att, out Guid obj_guid)
+        {
+            obj_guid = doc.Objects.Add(Value.IToRhino(), att);
+            return true;
+        }
     }
 }
---
+++ Alligator/NonComponents/Render/RenderBHoMGeometry.cs
@@ -0,0 +1,188 @@
+﻿using BH.Adapter.Rhinoceros;
+using BHG = BH.oM.Geometry;
+using RHG = Rhino.Geometry;
+using Grasshopper.Kernel;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.UI.Alligator
+{
+    public static partial class Render
+    {
+        /***************************************************/
+        /**** Public Methods  - Interfaces              ****/
+        /***************************************************/
+
+        public static void IRenderBHoMGeometry(this BHG.IBHoMGeometry geometry, GH_PreviewWireArgs args)
+        {
+            if (geometry == null) { return; }
+            args.Pipeline.ZBiasMode = 0;
+            Color bhColour = GetBHColor(args.Color);
+            try
+            {
+                RenderBHoMGeometry(geometry as dynamic, args.Pipeline, bhColour);
+            }
+            catch (Exception) { }
+        }
+
+        /***************************************************/
+
+        public static void IRenderBHoMGeometry(this BHG.IBHoMGeometry geometry, IGH_PreviewArgs args)
+        {
+            if (geometry == null) { return; }
+            args.Display.ZBiasMode = 0;
+            Color bhColour = GetBHColor(args.WireColour);
+            try
+            {
+                RenderBHoMGeometry(geometry as dynamic, args.Display, bhColour);
+            }
+            catch (Exception) { }
+        }
+
+
+        /***************************************************/
+        /**** Public Methods  - Vectors                 ****/
+        /***************************************************/
+
+        public static void RenderBHoMGeometry(BHG.Point point, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
+        {
+            pipeline.DrawPoint(point.ToRhino(), Rhino.Display.PointStyle.Simple, 4, bhColour);
+        }
+
+        /***************************************************/
+
+        public static void RenderBHoMGeometry(BHG.Vector vector, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
+        {
+            //args.Pipeline.DrawLineArrow(vector.ToRhino(), args.Color, args.Thickness, args.Thickness);
+        }
+
+        /***************************************************/
+
+        public static void RenderBHoMGeometry(BHG.Plane plane, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
+        {
+            pipeline.DrawConstructionPlane(new Rhino.DocObjects.ConstructionPlane() { Plane = (plane.ToRhino()), ThickLineColor = bhColour, ThinLineColor = Color.Black, GridLineCount = 10 });
+        }
+
+
+        /***************************************************/
+        /**** Public Methods  - Curves                  ****/
+        /***************************************************/
+
+        public static void RenderBHoMGeometry(BHG.Arc arc, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
+        {
+            pipeline.DrawArc(arc.ToRhino().Arc, bhColour, 3);
+        }
+
+        /***************************************************/
+
+        public static void RenderBHoMGeometry(BHG.Circle circle, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
+        {
+            pipeline.DrawCircle(circle.ToRhino(), bhColour, 3);
+        }
+
+        /***************************************************/
+
+        public static void RenderBHoMGeometry(BHG.Ellipse ellipse, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
+        {
+            pipeline.DrawCurve(ellipse.ToRhino().ToNurbsCurve(), bhColour, 3);
+        }
+
+        /***************************************************/
+
+        public static void RenderBHoMGeometry(BHG.Line line, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
+        {
+            pipeline.DrawLine(line.ToRhino().Line, bhColour, 3);
+        }
+        /***************************************************/
+
+        public static void RenderBHoMGeometry(BHG.NurbCurve curve, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
+        {
+            pipeline.DrawCurve(curve.ToRhino(), bhColour, 3);
+        }
+
+        /***************************************************/
+
+        public static void RenderBHoMGeometry(BHG.PolyCurve polycurve, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
+        {
+            pipeline.DrawCurve(polycurve.ToRhino(), bhColour, 3);
+        }
+
+        /***************************************************/
+
+        public static void RenderBHoMGeometry(BHG.Polyline polyline, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
+        {
+            pipeline.DrawPolyline(polyline.ControlPoints.Select(x => x.ToRhino()), bhColour, 3);
+        }
+
+
+        /***************************************************/
+        /**** Public Methods  - Surfaces                ****/
+        /***************************************************/
+
+        public static void RenderBHoMGeometry(BHG.BoundingBox bbBox, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
+        {
+            pipeline.DrawBox(bbBox.ToRhino(), bhColour, 3);
+        }
+
+        /***************************************************/
+
+        public static void RenderBHoMGeometry(BHG.NurbSurface surface, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
+        {
+            pipeline.DrawSurface(surface.ToRhino(), bhColour, 1);
+        }
+
+        /***************************************************/
+
+        public static void RenderBHoMGeometry(BHG.PolySurface polySurface, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
+        {
+            polySurface.Surfaces.ForEach(srf => pipeline.DrawSurface(srf.IToRhino(), bhColour, 1));
+        }
+
+
+        /***************************************************/
+        /**** Public Methods  - Mesh                    ****/
+        /***************************************************/
+
+        public static void RenderBHoMGeometry(BHG.Mesh mesh, GH_PreviewMeshArgs args)
+        {
+            RHG.Mesh rMesh = mesh.ToRhino();
+            args.Pipeline.DrawMeshWires(rMesh, args.Material.Diffuse);
+            args.Pipeline.DrawMeshShaded(rMesh, args.Material);
+        }
+
+
+        /***************************************************/
+        /**** Public Methods  - Miscellanea             ****/
+        /***************************************************/
+
+        public static void RenderBHoMGeometry(BHG.CompositeGeometry composite, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
+        {
+            if (composite.Elements.Count == 0) { return; }
+            foreach (BHG.IBHoMGeometry geom in composite.Elements)
+            {
+                try
+                {
+                    RenderBHoMGeometry(geom as dynamic, pipeline, bhColour);
+                }
+                catch (Exception) { }
+            }
+        }
+
+
+        /***************************************************/
+        /**** Private Methods  -                        ****/
+        /***************************************************/
+
+        private static Color GetBHColor(Color ghColor)
+        {
+            int R = ghColor.R - 59;      // Difference to BuroHappold Green
+            int G = ghColor.G + 168;     // Difference to BuroHappold Green
+            int B = ghColor.B;           // Difference to BuroHappold Green
+            return Color.FromArgb(100, R < 255 && R > 0 ? R : 0, G < 255 && G > 0 ? G : 255, B);
+        }
+    }
+}
