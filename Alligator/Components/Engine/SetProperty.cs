7450798bb12821c711602dd90f5509db000ba933
Arnaud Declercq <Arnaud.Declercq@burohappold.com>
2018-07-06 11:05:07 +0800

    Fix SetPRoperty for list properties

--- Alligator/Components/Engine/SetProperty.cs
+++ Alligator/Components/Engine/SetProperty.cs
@@ -3,6 +3,10 @@ using Grasshopper.Kernel;
 using Grasshopper.Kernel.Types;
 using BH.oM.Base;
 using BH.Engine.Reflection;
+using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BH.UI.Alligator.Base
 {
@@ -34,7 +38,7 @@ namespace BH.UI.Alligator.Base
         {
             pManager.AddParameter(new BHoMObjectParameter(), "BHoM object", "object", "BHoM object to convert", GH_ParamAccess.item);
             pManager.AddTextParameter("key", "key", "Property name", GH_ParamAccess.item);
-            pManager.AddGenericParameter("value", "value", "Property value", GH_ParamAccess.item);
+            pManager.AddScriptVariableParameter("value", "value", "Property value", GH_ParamAccess.item);
         }
 
         /*******************************************/
@@ -56,7 +60,26 @@ namespace BH.UI.Alligator.Base
 
             DA.GetData(0, ref obj);
             DA.GetData(1, ref key);
-            DA.GetData(2, ref value);
+
+            if (Params.Input[2].Access == GH_ParamAccess.item)
+                DA.GetData(2, ref value);
+            else // TODO: We need to take care of the case for list of list (not currently available as an option in the ScriptParam menu)
+            {
+                Type type = obj.GetType().GetProperty(key).PropertyType;
+                if (typeof(IEnumerable).IsAssignableFrom(type))
+                {
+                    type = type.GetGenericArguments().First();
+                    MethodInfo generic = DA.GetType().GetMethods().Where(x => x.Name == "GetDataList" && x.GetParameters().First().ParameterType == typeof(int)).First().MakeGenericMethod(type);
+                    object[] arguments = new object[] { 2, Activator.CreateInstance(typeof(List<>).MakeGenericType(type)) };
+                    generic.Invoke(DA, arguments);
+                    value = arguments[1];
+                }
+                else
+                {
+                    Engine.Reflection.Compute.RecordError("The property to set is not a list");
+                    return;
+                } 
+            }
 
             while (value is IGH_Goo)
                 value = ((IGH_Goo)value).ScriptVariable();
