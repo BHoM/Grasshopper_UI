563763122ed89347270bf98fdff0119ba2c8bc30
adecler <arnaud.declercq@burohappold.com>
2018-07-28 19:09:57 +0800

    Fixing #230 - Lists not working as input for Tags in SetProperty

--- Alligator/Components/Engine/SetProperty.cs
+++ Alligator/Components/Engine/SetProperty.cs
@@ -61,25 +61,18 @@ namespace BH.UI.Alligator.Base
             DA.GetData(0, ref obj);
             DA.GetData(1, ref key);
 
-            if (Params.Input[2].Access == GH_ParamAccess.item)
-                DA.GetData(2, ref value);
-            else // TODO: We need to take care of the case for list of list (not currently available as an option in the ScriptParam menu)
+            Type type = obj.GetType().GetProperty(key).PropertyType;
+
+            if (Params.Input[2].Access == GH_ParamAccess.list) // TODO: We need to take care of the case for list of list (not currently available as an option in the ScriptParam menu)
             {
-                Type type = obj.GetType().GetProperty(key).PropertyType;
-                if (typeof(IEnumerable).IsAssignableFrom(type))
-                {
-                    type = type.GetGenericArguments().First();
-                    MethodInfo generic = DA.GetType().GetMethods().Where(x => x.Name == "GetDataList" && x.GetParameters().First().ParameterType == typeof(int)).First().MakeGenericMethod(type);
-                    object[] arguments = new object[] { 2, Activator.CreateInstance(typeof(List<>).MakeGenericType(type)) };
-                    generic.Invoke(DA, arguments);
-                    value = arguments[1];
-                }
-                else
-                {
-                    Engine.Reflection.Compute.RecordError("The property to set is not a list");
-                    return;
-                } 
+                Type subType = type.GetGenericArguments().First();
+                MethodInfo generic = DA.GetType().GetMethods().Where(x => x.Name == "GetDataList" && x.GetParameters().First().ParameterType == typeof(int)).First().MakeGenericMethod(subType);
+                object[] arguments = new object[] { 2, Activator.CreateInstance(typeof(List<>).MakeGenericType(subType)) };
+                generic.Invoke(DA, arguments);
+                value = arguments[1];
             }
+            else
+                DA.GetData(2, ref value);
 
             while (value is IGH_Goo)
                 value = ((IGH_Goo)value).ScriptVariable();
@@ -87,8 +80,26 @@ namespace BH.UI.Alligator.Base
             if (value.GetType().Namespace.StartsWith("Rhino.Geometry"))
                 value = Engine.Rhinoceros.Convert.ToBHoM(value as dynamic);
 
+            if (value.GetType() != type)
+            {
+                ConstructorInfo constructor = type.GetConstructor(new Type[] { value.GetType() });
+                if (constructor != null)
+                    value = constructor.Invoke(new object[] { value });
+            }
+
             IBHoMObject newObject = obj.GetShallowClone();
-            newObject.SetPropertyValue(key, value);
+
+            try
+            {
+                newObject.SetPropertyValue(key, value);
+            }
+            catch (Exception e)
+            {
+                if (Params.Input[2].Access == GH_ParamAccess.item)
+                    Compute.RecordError(e.ToString() + "\nHave you forgotten to mark the input as a List in its menu?");
+                else
+                    Compute.RecordError(e.ToString());
+            }
 
             DA.SetData(0, newObject);
 
