607fb13e0f2972c14cc4f43b91da42865c94ceb2
Arnaud Declercq <arnaud.declercq@burohappold.com>
2018-11-29 09:47:57 +0800

    Avoid confusion by deactivating old global search menu

--- Alligator/NonComponents/Menus/SearchPopup.cs
+++ Alligator/NonComponents/Menus/SearchPopup.cs
@@ -1,50 +1,50 @@
-﻿using Grasshopper.GUI.Canvas;
-using System;
-using System.IO;
-using System.Reflection;
+﻿//using Grasshopper.GUI.Canvas;
+//using System;
+//using System.IO;
+//using System.Reflection;
 
-namespace BH.UI.Alligator.Menus
-{
-    public static class SearchPopup
-    {
-        /*******************************************/
-        /**** Public Methods                    ****/
-        /*******************************************/
+//namespace BH.UI.Alligator.Menus
+//{
+//    public static class SearchPopup
+//    {
+//        /*******************************************/
+//        /**** Public Methods                    ****/
+//        /*******************************************/
 
-        public static void Activate()
-        {
-            Grasshopper.Instances.CanvasCreated += Instances_CanvasCreated;
-        }
+//        public static void Activate()
+//        {
+//            Grasshopper.Instances.CanvasCreated += Instances_CanvasCreated;
+//        }
 
 
-        /*******************************************/
-        /**** Event Methods                     ****/
-        /*******************************************/
+//        /*******************************************/
+//        /**** Event Methods                     ****/
+//        /*******************************************/
 
-        public static void Instances_CanvasCreated(GH_Canvas canvas)
-        {
-            string folder = @"C:\Users\" + Environment.UserName + @"\AppData\Roaming\Grasshopper\Libraries\Alligator\";
-            BH.Engine.Reflection.Compute.LoadAllAssemblies(folder);
+//        public static void Instances_CanvasCreated(GH_Canvas canvas)
+//        {
+//            string folder = @"C:\Users\" + Environment.UserName + @"\AppData\Roaming\Grasshopper\Libraries\Alligator\";
+//            BH.Engine.Reflection.Compute.LoadAllAssemblies(folder);
 
-            canvas.KeyDown += ActiveCanvas_KeyDown;
-        }
+//            canvas.KeyDown += ActiveCanvas_KeyDown;
+//        }
 
-        /*******************************************/
+//        /*******************************************/
 
-        public static void ActiveCanvas_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
-        {
-            bool modCtrl = (System.Windows.Forms.Control.ModifierKeys & System.Windows.Forms.Keys.Control) == System.Windows.Forms.Keys.Control;
-            bool modShift = (System.Windows.Forms.Control.ModifierKeys & System.Windows.Forms.Keys.Shift) == System.Windows.Forms.Keys.Shift;
+//        public static void ActiveCanvas_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+//        {
+//            bool modCtrl = (System.Windows.Forms.Control.ModifierKeys & System.Windows.Forms.Keys.Control) == System.Windows.Forms.Keys.Control;
+//            bool modShift = (System.Windows.Forms.Control.ModifierKeys & System.Windows.Forms.Keys.Shift) == System.Windows.Forms.Keys.Shift;
 
-            if (e.KeyCode == System.Windows.Forms.Keys.B && modCtrl && !modShift)
-            {
-                GH_Canvas canvas = Grasshopper.Instances.ActiveCanvas;
-                System.Drawing.Point position = System.Windows.Forms.Cursor.Position;
+//            if (e.KeyCode == System.Windows.Forms.Keys.B && modCtrl && !modShift)
+//            {
+//                GH_Canvas canvas = Grasshopper.Instances.ActiveCanvas;
+//                System.Drawing.Point position = System.Windows.Forms.Cursor.Position;
 
-                new BH_PopupSearchDialog { BasePoint = position, Canvas = canvas }.Show(canvas.FindForm());
-            }
-        }
+//                new BH_PopupSearchDialog { BasePoint = position, Canvas = canvas }.Show(canvas.FindForm());
+//            }
+//        }
 
-        /*******************************************/
-    }
-}
+//        /*******************************************/
+//    }
+//}
