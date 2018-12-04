e6917f7154e3a8a618047d5088d650bc234d20ec
Eduardo Pignatelli <Eduardo.Pignatelli@BuroHappold.com>
2018-12-04 14:10:26 +0000

    Fixes #249

---
+++ Grasshopper_Engine/Compute/DrawViewportWires.cs
@@ -0,0 +1,44 @@
+﻿using BH.Engine.Rhinoceros;
+using BHG = BH.oM.Geometry;
+using RHG = Rhino.Geometry;
+using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+using System.Linq;
+using BH.Engine.Base;
+using BH.oM.Geometry;
+
+namespace BH.Engine.Alligator
+{
+    public static partial class Compute
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static void DrawViewportMeshes(IGeometry geom, GH_PreviewMeshArgs args)
+        {
+            if (geom == null)
+            {
+                return;
+            }
+            if (geom is BH.oM.Geometry.Mesh)
+            {
+                Compute.Render((BH.oM.Geometry.Mesh)geom, args);
+            }
+        }
+
+        /***************************************************/
+
+        public static void DrawViewportWires(IGeometry geom, GH_PreviewWireArgs args)
+        {
+            if (geom == null)
+            {
+                return;
+            }
+            Compute.IRender(geom as dynamic, args);
+        }
+
+        /***************************************************/
+    }
+}
---
+++ Grasshopper_Engine/Query/RenderColor.cs
@@ -0,0 +1,29 @@
+﻿using GH = Grasshopper;
+using System.Drawing;
+
+namespace BH.Engine.Alligator
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static Color RenderColour(Color ghColour)
+        {
+            Color pColour = GH.Instances.ActiveCanvas.Document.PreviewColour;
+            if (ghColour.R == pColour.R & // If the color sent by PreviewArgs is the default object PreviewColour
+                ghColour.G == pColour.G &
+                ghColour.B == pColour.B) // Excluding Alpha channel from comparison
+            {
+                return Color.FromArgb(80, 255, 41, 105);
+            }
+            else
+            {
+                return ghColour;
+            }
+        }
+
+        /***************************************************/
+    }
+}
