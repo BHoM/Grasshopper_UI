a16ef5da602c4b30cdec2ec0dbfd3d039fdf19fc
Eduardo Pignatelli <Eduardo.Pignatelli@BuroHappold.com>
2018-12-06 11:18:05 +0000

    Separated mesh rendering from wire rendering

--- Grasshopper_Engine/Compute/RenderWires.cs
+++ Grasshopper_Engine/Compute/RenderWires.cs
@@ -15,14 +15,14 @@ namespace BH.Engine.Alligator
         /**** Public Methods  - Interfaces              ****/
         /***************************************************/
 
-        public static bool IRender(this BH.oM.Base.IBHoMObject bhObject, GH_PreviewWireArgs args)
+        public static bool IRenderWires(this BH.oM.Base.IBHoMObject bhObject, GH_PreviewWireArgs args)
         {
             if (bhObject == null) { return false; }
             args.Pipeline.ZBiasMode = 0;
             Color bhColour = Query.RenderColour(args.Color);
             try
             {
-                return Render(bhObject as dynamic, args, bhColour);
+                return RenderWires(bhObject as dynamic, args, bhColour);
             }
             catch (Exception) { }
             return false;
@@ -30,28 +30,28 @@ namespace BH.Engine.Alligator
 
         /***************************************************/
 
-        public static void IRender(this BHG.IGeometry geometry, GH_PreviewWireArgs args)
+        public static void IRenderWires(this BHG.IGeometry geometry, GH_PreviewWireArgs args)
         {
             if (geometry == null) { return; }
             args.Pipeline.ZBiasMode = 0;
             Color bhColour = Query.RenderColour(args.Color);
             try
             {
-                Render(geometry as dynamic, args.Pipeline, bhColour);
+                RenderWires(geometry as dynamic, args.Pipeline, bhColour);
             }
             catch (Exception) { }
         }
 
         /***************************************************/
 
-        public static void IRender(this BHG.IGeometry geometry, IGH_PreviewArgs args)
+        public static void IRenderWires(this BHG.IGeometry geometry, IGH_PreviewArgs args)
         {
             if (geometry == null) { return; }
             args.Display.ZBiasMode = 0;
             Color bhColour = Query.RenderColour(args.WireColour);
             try
             {
-                Render(geometry as dynamic, args.Display, bhColour);
+                RenderWires(geometry as dynamic, args.Display, bhColour);
             }
             catch (Exception) { }
         }
@@ -61,9 +61,9 @@ namespace BH.Engine.Alligator
         /**** Public Methods  - Objects                 ****/
         /***************************************************/
 
-        public static bool Render(BH.oM.Base.BHoMObject obj, GH_PreviewWireArgs args, Color bhColour)
+        public static bool RenderWires(BH.oM.Base.BHoMObject obj, GH_PreviewWireArgs args, Color bhColour)
         {
-            IRender(obj.IGeometry(), args);
+            IRenderWires(obj.IGeometry(), args);
             return true;
         }
 
@@ -72,28 +72,28 @@ namespace BH.Engine.Alligator
         /**** Public Methods  - Vectors                 ****/
         /***************************************************/
 
-        public static void Render(BHG.Point point, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
+        public static void RenderWires(BHG.Point point, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
         {
             pipeline.DrawPoint(point.ToRhino(), Rhino.Display.PointStyle.Simple, 4, bhColour);
         }
 
         /***************************************************/
 
-        public static void Render(BHG.Vector vector, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
+        public static void RenderWires(BHG.Vector vector, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
         {
             //args.Pipeline.DrawLineArrow(vector.ToRhino(), args.Color, args.Thickness, args.Thickness);
         }
 
         /***************************************************/
 
-        public static void Render(BHG.Plane plane, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
+        public static void RenderWires(BHG.Plane plane, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
         {
             pipeline.DrawConstructionPlane(new Rhino.DocObjects.ConstructionPlane() { Plane = (plane.ToRhino()), ThickLineColor = bhColour, ThinLineColor = Color.Black, GridLineCount = 10 });
         }
 
         /***************************************************/
 
-        public static void Render(BHG.CoordinateSystem coordinateSystem, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
+        public static void RenderWires(BHG.CoordinateSystem coordinateSystem, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
         {
             pipeline.DrawConstructionPlane(new Rhino.DocObjects.ConstructionPlane() { Plane = (coordinateSystem.ToRhino()), ThickLineColor = bhColour, ThinLineColor = Color.Black, GridLineCount = 10 });
         }
@@ -103,48 +103,48 @@ namespace BH.Engine.Alligator
         /**** Public Methods  - Curves                  ****/
         /***************************************************/
 
-        public static void Render(BHG.Arc arc, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
+        public static void RenderWires(BHG.Arc arc, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
         {
             pipeline.DrawArc(arc.ToRhino(), bhColour, 2);
         }
 
         /***************************************************/
 
-        public static void Render(BHG.Circle circle, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
+        public static void RenderWires(BHG.Circle circle, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
         {
             pipeline.DrawCircle(circle.ToRhino(), bhColour, 2);
         }
 
         /***************************************************/
 
-        public static void Render(BHG.Ellipse ellipse, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
+        public static void RenderWires(BHG.Ellipse ellipse, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
         {
             pipeline.DrawCurve(ellipse.ToRhino().ToNurbsCurve(), bhColour, 2);
         }
 
         /***************************************************/
 
-        public static void Render(BHG.Line line, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
+        public static void RenderWires(BHG.Line line, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
         {
             pipeline.DrawLine(line.ToRhino(), bhColour, 2);
         }
         /***************************************************/
 
-        public static void Render(BHG.NurbCurve curve, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
+        public static void RenderWires(BHG.NurbCurve curve, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
         {
             pipeline.DrawCurve(curve.ToRhino(), bhColour, 2);
         }
 
         /***************************************************/
 
-        public static void Render(BHG.PolyCurve polycurve, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
+        public static void RenderWires(BHG.PolyCurve polycurve, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
         {
             pipeline.DrawCurve(polycurve.ToRhino(), bhColour, 2);
         }
 
         /***************************************************/
 
-        public static void Render(BHG.Polyline polyline, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
+        public static void RenderWires(BHG.Polyline polyline, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
         {
             pipeline.DrawPolyline(polyline.ControlPoints.Select(x => x.ToRhino()), bhColour, 2);
         }
@@ -154,37 +154,40 @@ namespace BH.Engine.Alligator
         /**** Public Methods  - Surfaces                ****/
         /***************************************************/
 
-        public static void Render(BHG.Extrusion surface, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
+        public static void RenderWires(BHG.Extrusion surface, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
         {
-            pipeline.DrawSurface(surface.ToRhino(), bhColour, 1);
+            RHG.Surface rSurface = surface.ToRhino();
+            pipeline.DrawSurface(rSurface, bhColour, 2);
         }
 
         /***************************************************/
 
-        public static void Render(BHG.Loft surface, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
+        public static void RenderWires(BHG.Loft surface, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
         {
-            pipeline.DrawSurface(surface.ToRhino(), bhColour, 1);
+            RHG.Surface rSurface = surface.ToRhino();
+            pipeline.DrawSurface(rSurface, bhColour, 2);
         }
 
         /***************************************************/
 
-        public static void Render(BHG.NurbSurface surface, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
+        public static void RenderWires(BHG.NurbSurface surface, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
         {
-            pipeline.DrawSurface(surface.ToRhino(), bhColour, 1);
+            RHG.Surface rSurface = surface.ToRhino();
+            pipeline.DrawSurface(rSurface, bhColour, 2);
         }
 
         /***************************************************/
 
-        public static void Render(BHG.Pipe surface, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
+        public static void RenderWires(BHG.Pipe surface, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
         {
-            pipeline.DrawBrepShaded(surface.ToRhino(), new Rhino.Display.DisplayMaterial(bhColour));
+            pipeline.DrawBrepWires(surface.ToRhino(), bhColour, 0);
         }
 
         /***************************************************/
 
-        public static void Render(BHG.PolySurface polySurface, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
+        public static void RenderWires(BHG.PolySurface polySurface, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
         {
-            polySurface.Surfaces.ForEach(srf => pipeline.DrawSurface(srf.IToRhino(), bhColour, 1));
+            polySurface.Surfaces.ForEach(srf => pipeline.DrawSurface(srf.IToRhino(), bhColour, 2));
         }
 
 
@@ -192,41 +195,31 @@ namespace BH.Engine.Alligator
         /**** Public Methods  - Mesh                    ****/
         /***************************************************/
 
-        public static void Render(BHG.Mesh mesh, GH_PreviewMeshArgs args)
-        {
-            RHG.Mesh rMesh = mesh.ToRhino();
-            args.Pipeline.DrawMeshWires(rMesh, args.Material.Diffuse);
-            args.Pipeline.DrawMeshShaded(rMesh, args.Material);
-        }
-
-        /***************************************************/
-
-        public static void Render(BHG.Mesh mesh, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
+        public static void RenderWires(BHG.Mesh mesh, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
         {
             RHG.Mesh rMesh = mesh.ToRhino();
             pipeline.DrawMeshWires(rMesh, bhColour);
-            pipeline.DrawMeshShaded(rMesh, new Rhino.Display.DisplayMaterial(bhColour));
         }
 
         /***************************************************/
         /**** Public Methods  - Miscellanea             ****/
         /***************************************************/
 
-        public static void Render(BHG.BoundingBox bbBox, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
+        public static void RenderWires(BHG.BoundingBox bbBox, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
         {
-            pipeline.DrawBox(bbBox.ToRhino(), bhColour, 2);
+            pipeline.DrawLines(bbBox.ToRhino().GetEdges(), bhColour, 2);
         }
 
         /***************************************************/
 
-        public static void Render(BHG.CompositeGeometry composite, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
+        public static void RenderWires(BHG.CompositeGeometry composite, Rhino.Display.DisplayPipeline pipeline, Color bhColour)
         {
             if (composite.Elements.Count == 0) { return; }
             foreach (BHG.IGeometry geom in composite.Elements)
             {
                 try
                 {
-                    Render(geom as dynamic, pipeline, bhColour);
+                    RenderWires(geom as dynamic, pipeline, bhColour);
                 }
                 catch (Exception) { }
             }
---
+++ Grasshopper_Engine/Query/RenderMaterial.cs
@@ -0,0 +1,31 @@
+﻿using GH = Grasshopper;
+using System.Drawing;
+using Rhino.Display;
+
+namespace BH.Engine.Alligator
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static DisplayMaterial RenderMaterial(DisplayMaterial material)
+        {
+            Color pColour = GH.Instances.ActiveCanvas.Document.PreviewColour;
+            Color ghColour = material.Diffuse;
+            if (ghColour.R == pColour.R & // If the color sent by PreviewArgs is the default object PreviewColour
+                ghColour.G == pColour.G &
+                ghColour.B == pColour.B) // Excluding Alpha channel from comparison
+            {
+                return new DisplayMaterial(Color.FromArgb(255, 255, 41, 105), 0.6);
+            }
+            else
+            {
+                return material;
+            }
+        }
+
+        /***************************************************/
+    }
+}
