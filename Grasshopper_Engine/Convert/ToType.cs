ba27b97575bda4f0fb8f1acffce408fc9319c20b
Isak Naslund <Isak.Naslund@burohappold.com>
2018-12-14 11:45:31 +0100

    Fixes #287

--- Grasshopper_Engine/Convert/ToType.cs
+++ Grasshopper_Engine/Convert/ToType.cs
@@ -21,6 +21,17 @@ namespace BH.Engine.Grasshopper
             if (type != null)
                 return type;
 
+            if (typeString.Contains("[[") && typeString.Contains("]]"))
+            {
+                int outerSplit = typeString.IndexOf("[[");
+                int innerLength = typeString.LastIndexOf("]]") - outerSplit - 2;
+
+                Type outer = typeString.Substring(0, outerSplit).ToType();
+                Type inner = typeString.Substring(outerSplit + 2, innerLength).ToType();
+
+                return outer.MakeGenericType(new Type[] { inner });
+            }
+
             // Try get to recover type from full name
             string[] parts = typeString.Split(new char[] { ',' });
             if (parts.Length == 0)
