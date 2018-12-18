8c2fb0371253832a8bc19e4964f551f531ad0e25
Eduardo Pignatelli <Eduardo.Pignatelli@burohappold.com>
2018-12-18 15:09:03 +0000

    Rename project, assembly name and default namespace

--- Alligator/Properties/Resources.Designer.cs
+++ Alligator/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace BH.UI.Alligator.Base.Properties {
+namespace BH.UI.Grasshopper.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace BH.UI.Alligator.Base.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BH.UI.Alligator.Base.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BH.UI.Grasshopper.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
