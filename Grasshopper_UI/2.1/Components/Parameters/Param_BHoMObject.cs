47892a36e5f48aa7675b053df7f95fb90ca927c5
Eduardo Pignatelli <Eduardo.Pignatelli@BuroHappold.com>
2019-03-19 12:50:39 +0000

    Param_BHoMObject imheriting from BHoMParam

--- Grasshopper_UI/2.1/Components/Parameters/Param_BHoMObject.cs
+++ Grasshopper_UI/2.1/Components/Parameters/Param_BHoMObject.cs
@@ -1,6 +1,6 @@
 /*
  * This file is part of the Buildings and Habitats object Model (BHoM)
- * Copyright (c) 2015 - 2018, the respective contributors. All rights reserved.
+ * Copyright (c) 2015 - 2019, the respective contributors. All rights reserved.
  *
  * Each contributor holds copyright over their respective contributions.
  * The project versioning (Git) records all such contribution source information.
@@ -21,18 +21,12 @@
  */
 
 using BH.UI.Grasshopper.Properties;
-using BH.UI.Grasshopper.Templates;
-using BH.UI.Templates;
-using GH_IO.Serialization;
 using Grasshopper.Kernel;
 using System;
-using System.Collections.Generic;
-using System.Reflection;
-using System.Runtime.CompilerServices;
 
 namespace BH.UI.Grasshopper.Objects
 {
-    public class Param_BHoMObject : GH_PersistentParam<Engine.Grasshopper.Objects.GH_BHoMObject>, IGH_PreviewObject
+    public class Param_BHoMObject : BHoMParam<Engine.Grasshopper.Objects.GH_BHoMObject>
     {
         /*******************************************/
         /**** Properties                        ****/
@@ -40,104 +34,17 @@ namespace BH.UI.Grasshopper.Objects
 
         protected override System.Drawing.Bitmap Icon { get; } = Resources.BHoMObject_Param;
 
-        public override GH_Exposure Exposure { get; } = GH_Exposure.tertiary;
-
         public override Guid ComponentGuid { get; } = new Guid("1DEF3710-FD5B-4617-BCF6-B6293C5C6530");
 
         public override string TypeName { get; } = "BHoM Object";
-
-        public bool Hidden { get; set; } = false;
-
-        public bool IsPreviewCapable { get; } = true;
-
-        public Rhino.Geometry.BoundingBox ClippingBox { get { return Preview_ComputeClippingBox(); } }
 
 
         /*******************************************/
         /**** Constructors                      ****/
         /*******************************************/
 
-        public Param_BHoMObject()
-            : base(new GH_InstanceDescription("BHoM object", "BHoM", "Represents a collection of generic BHoM objects", "Params", "Primitive"))
+        public Param_BHoMObject() : base("BHoM object", "BHoM", "Represents a collection of generic BHoM objects", "Params", "Primitive")
         {
-        }
-
-
-        /*******************************************/
-        /**** Override Methods                  ****/
-        /*******************************************/
-
-        public void DrawViewportMeshes(IGH_PreviewArgs args)
-        {
-            Preview_DrawMeshes(args);
-        }
-
-        /*******************************************/
-
-        public void DrawViewportWires(IGH_PreviewArgs args)
-        {
-            Preview_DrawWires(args);
-        }
-
-        /*******************************************/
-
-        public override bool Read(GH_IReader reader)
-        {
-            Engine.Reflection.Compute.ClearCurrentEvents();
-            bool success = base.Read(reader);
-            Logging.ShowEvents(this, Engine.Reflection.Query.CurrentEvents());
-            return success;
-        }
-
-        /*******************************************/
-
-        protected override GH_GetterResult Prompt_Singular(ref Engine.Grasshopper.Objects.GH_BHoMObject value)
-        {
-            return GH_GetterResult.cancel;
-        }
-
-        /*******************************************/
-
-        protected override GH_GetterResult Prompt_Plural(ref List<Engine.Grasshopper.Objects.GH_BHoMObject> values)
-        {
-            return GH_GetterResult.cancel;
-        }
-
-        /*******************************************/
-
-        protected override string HtmlHelp_Source()
-        {
-            if (this.Attributes.IsTopLevel)
-            {
-                Engine.Grasshopper.Objects.GH_BHoMObject obj = this.m_data.get_FirstItem(true);
-                if (obj != null && obj.Value != null)
-                {
-                    BH.Engine.Reflection.Compute.OpenHelpPage(obj.Value.GetType());
-                }
-            }
-            else if (typeof(CallerComponent).IsAssignableFrom(this.Attributes.GetTopLevel.DocObject.GetType()))
-            {
-                CallerComponent parent = this.Attributes.GetTopLevel.DocObject as CallerComponent;
-                if (parent.Caller != null && parent.Caller.SelectedItem is MethodInfo)
-                {
-                    MethodInfo method = parent.Caller.SelectedItem as MethodInfo;
-                    Type type;
-                    if (parent.Params.IsInputParam(this))
-                    {
-                        ParameterInfo[] inputs = method.GetParameters();
-                        int k = parent.Params.Input.FindIndex(p => p.NickName == this.NickName);
-                        if (k < inputs.Length)
-                            type = inputs[k].ParameterType;
-                    }
-                    else if (parent.Params.IsOutputParam(this))
-                    {
-                        type = method.ReflectedType;
-                    }
-
-                    BH.Engine.Reflection.Compute.OpenHelpPage(method.ReturnType);
-                }
-            }
-            return base.HtmlHelp_Source();
         }
     }
 
