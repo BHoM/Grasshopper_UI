ba84323d01ab1c26eeeb3280df684cfeac3e7d46
Eduardo Pignatelli <Eduardo.Pignatelli@BuroHappold.com>
2019-09-20 09:38:44 +0100

    Add Param_BHoMAdapter

---
+++ Grasshopper_UI/2.1/Components/Parameters/Param_BHoMAdapter.cs
@@ -0,0 +1,52 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2019, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.UI.Grasshopper.Properties;
+using BH.UI.Grasshopper.Templates;
+using System;
+
+namespace BH.UI.Grasshopper.Parameters
+{
+    public class Param_BHoMAdapter : BHoMParam<Engine.Grasshopper.Objects.BHoMAdaptertGoo>
+    {
+        /*******************************************/
+        /**** Properties                        ****/
+        /*******************************************/
+
+        protected override System.Drawing.Bitmap Icon { get; } = Resources.BHoMObject_Param;
+
+        public override Guid ComponentGuid { get; } = new Guid("1DEF3710-FD5B-4617-BCF6-B6293C5C6539");
+
+        public override string TypeName { get; } = "BHoMAdapter";
+
+
+        /*******************************************/
+        /**** Constructors                      ****/
+        /*******************************************/
+
+        public Param_BHoMAdapter() : base("BHoM Adapter", "BHoMAdapter", "A BHoM Adapter or an implementation of it", "Params", "Primitive")
+        {
+        }
+        
+        /*******************************************/
+    }
+}
