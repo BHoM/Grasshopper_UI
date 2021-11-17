16be666cf69cb6b7c34491a7a46cd0b02f4b4fb5
adecler <arnaud.declercq@burohappold.com>
2021-11-17 14:38:21 +0800

    Adding UI Setting with ability to disable wire menu

---
+++ Grasshopper_oM/UISettings.cs
@@ -0,0 +1,16 @@
+﻿using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Grasshopper
+{
+    [Description("Settings for the Grasshopper UI.")]
+    public class UISettings : BHoMObject, ISettings
+    {
+        public virtual bool UseWireMenu { get; set; } = true;
+    }
+}
