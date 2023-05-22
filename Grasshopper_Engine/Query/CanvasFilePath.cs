1acd16bfe91a08ea3f7b76a29b679fd618171f3d
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2023-05-22 16:07:51 +0100

    Add a Query method to get the currently active canvas file path

---
+++ Grasshopper_Engine/Query/CanvasFilePath.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GH = Grasshopper;
+using Grasshopper.Kernel;
+using System.ComponentModel;
+using BH.oM.Base.Attributes;
+
+namespace BH.Engine.Grasshopper
+{
+    public static partial class Query
+    {
+        [Description("Gets the saved file path of the currently open Grasshopper Document for the active canvas.")]
+        [Output("filePath", "The local file path for the active canvas.")]
+        public static string CanvasFilePath()
+        {
+            var ghDoc = GH.Instances.ActiveCanvas.Document;
+            if (ghDoc != null && ghDoc.IsFilePathDefined)
+                return ghDoc.FilePath;
+
+            BH.Engine.Base.Compute.RecordWarning("Document has not been saved and does not yet have a file path defined.");
+            return "";
+        }
+    }
+}
